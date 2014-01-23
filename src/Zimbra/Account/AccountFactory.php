1c17cf1de7cae392ff76a2321b3f0861259f00cd
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2014-01-23 23:39:46 +0700

    Initialize Zimbra Account classes

---
+++ src/Zimbra/Account/AccountFactory.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Account;
+
+/**
+ * Account is a class which allows to manage account in Zimbra
+ * 
+ * @package   Zimbra
+ * @category  Account
+ * @author    Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright Copyright © 2013 by Nguyen Van Nguyen.
+ */
+abstract class AccountFactory
+{
+    /**
+     * @var array
+     */
+    private static $_instances = array();
+
+    /**
+     * Creates a singleton of a AccountInterface base on parameters.
+     *
+     * @param  string $location The Zimbra api soap location.
+     * @param  string $client   Soap client
+     * @return AccountInterface
+     */
+    public static function instance($location = 'https://localhost/service/soap', $client = 'http')
+    {
+        $key = md5($location.$client);
+        if (isset(self::$_instances[$key]) and (self::$_instances[$key] instanceof AccountInterface))
+        {
+            return self::$_instances[$key];
+        }
+        else
+        {
+            self::$_instances[$key] = self::factory($location, $client);
+            return self::$_instances[$key];            
+        }
+    }
+
+    /**
+     * Returns a new AccountInterface object.
+     *
+     * @param  string $location The Zimbra api soap location.
+     * @param  string $client   Soap client
+     * @return AccountInterface
+     */
+    public static function factory($location = 'https://localhost/service/soap', $client = 'http')
+    {
+        switch (strtolower($client))
+        {
+            case 'wsdl':
+                return new Wsdl($location);
+            default:
+                return new Http($location);
+        }
+    }
+}
