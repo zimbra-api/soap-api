0568840d13633fed66a506bb334aba41350e4fb6
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2014-01-27 22:34:21 +0700

    Initialize Zimbra Mail classes
    Initialize Zimbra Mail classes

---
+++ src/Zimbra/Mail/Http.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail;
+
+use Zimbra\Soap\Client\Http as ClientHttp;
+
+/**
+ * Http is a class which allows to connect Zimbra API mail functions via SOAP using pecl_http extension
+ * @package   Zimbra
+ * @category  API
+ * @author    Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright Copyright © 2013 by Nguyen Van Nguyen.
+ */
+class Http extends Base
+{
+    /**
+     * Http constructor.
+     *
+     * @param string $location The Zimbra api soap location.
+     */
+    public function __construct($location)
+    {
+        parent::__construct($location);
+        $this->_client = new ClientHttp($this->_location, $this->_namespace);
+    }
+}
