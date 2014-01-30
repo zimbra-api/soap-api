c6d2ecbd6132da5ed47442bcdfafe0743e64d2bb
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2014-01-30 22:09:28 +0700

    Initialize Zimbra Voice classes

---
+++ src/Zimbra/Voice/Base.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\API\Voice;
+
+use Zimbra\Soap\API;
+
+/**
+ * Base is a abstract class which allows to connect Zimbra API mail public functions via SOAP
+ * @package   Zimbra
+ * @category  API
+ * @author    Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright Copyright © 2013 by Nguyen Van Nguyen.
+ */
+abstract class Base extends API implements VoiceInterface
+{
+    /**
+     * Base constructor
+     *
+     * @param string $location The Zimbra api soap location.
+     */
+    public function __construct($location)
+    {
+        $this->_location = $location;
+        $this->_namespace = 'urn:zimbraVoice';
+    }
+}
---
+++ src/Zimbra/Voice/VoiceFactory.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Voice;
+
+/**
+ * Voice soap api factory class
+ * 
+ * @package   Zimbra
+ * @category  Mail
+ * @author    Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright Copyright © 2013 by Nguyen Van Nguyen.
+ */
+abstract class VoiceFactory
+{
+    /**
+     * @var array
+     */
+    private static $_instances = array();
+
+    /**
+     * Creates a singleton of a VoiceInterface base on parameters.
+     *
+     * @param  string $location The Zimbra api soap location.
+     * @param  string $client   Soap client
+     * @return VoiceInterface
+     */
+    public static function instance($location = 'https://localhost/service/soap', $client = 'http')
+    {
+        $key = md5($location.$client);
+        if (isset(self::$_instances[$key]) and (self::$_instances[$key] instanceof VoiceInterface))
+        {
+            return self::$_instances[$key];
+        }
+        else
+        {
+            self::$_instances[$key] = self::factory($location, $client);
+            return self::$_instances[$key];            
+        }
+    }
+
+    /**
+     * Returns a new VoiceInterface object.
+     *
+     * @param  string $location The Zimbra api soap location.
+     * @param  string $client   Soap client
+     * @return VoiceInterface
+     */
+    public static function factory($location = 'https://localhost/service/soap', $client = 'http')
+    {
+        switch (strtolower($client))
+        {
+            case 'wsdl':
+                return new Wsdl($location);
+            default:
+                return new Http($location);
+        }
+    }
+}
