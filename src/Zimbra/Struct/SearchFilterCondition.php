a600053f109e025207248fea8c62145f6935b4d4
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2014-03-02 16:14:08 +0700

    Add SearchFilterCondition interface

---
+++ src/Zimbra/Struct/SearchFilterCondition.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Struct;
+use Zimbra\Struct\SearchFilterCondition;
+
+/**
+ * SearchFilterCondition interface
+ *
+ * @package   Zimbra
+ * @category  Struct
+ * @author    Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright Copyright © 2013 by Nguyen Van Nguyen.
+ */
+interface SearchFilterCondition
+{
+    /**
+     * Gets or sets not flag
+     *
+     * @param  bool $not
+     * @return bool|self
+     */
+	function notFlag($not = null);
+}
