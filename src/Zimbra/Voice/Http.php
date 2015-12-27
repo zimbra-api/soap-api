ddef22302540e0f4e16b4c02e6d8e2a6da16a421
nguyennv <nguyennv1981@gmail.com>
2015-12-27 21:43:32 +0700

    Update zimbra voice

--- src/Zimbra/Voice/Http.php
+++ src/Zimbra/Voice/Http.php
@@ -32,10 +32,10 @@ class Http extends Base
     public function __construct($location, RequestFormat $format = null)
     {
         parent::__construct($location);
-        $this->_client = ClientHttp::instance($this->_location);
+        $this->setClient(ClientHttp::instance($this->getLocation()));
         if($format instanceof RequestFormat)
         {
-            $this->_client->format($format);
+            $this->getClient()->format($format);
         }
     }
 }
