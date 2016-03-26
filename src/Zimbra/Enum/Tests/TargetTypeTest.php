2daf1b682bc2521958679b4ccbeea6aa16c01eb6
nguyennv <nguyennv1981@gmail.com>
2016-03-26 12:18:20 +0700

    Update test case for TargetType enum

--- src/Zimbra/Enum/Tests/TargetTypeTest.php
+++ src/Zimbra/Enum/Tests/TargetTypeTest.php
@@ -13,18 +13,19 @@ class TargetTypeTest extends PHPUnit_Framework_TestCase
     public function testTargetType()
     {
         $values = [
-            'ACCOUNT'       => 'account',
-            'CALRESOURCE'   => 'calresource',
-            'COS'           => 'cos',
-            'DL'            => 'dl',
-            'GROUP'         => 'group',
-            'DOMAIN'        => 'domain',
-            'SERVER'        => 'server',
-            'UCSERVICE'     => 'ucservice',
-            'XMPPCOMPONENT' => 'xmppcomponent',
-            'ZIMLET'        => 'zimlet',
-            'CONFIG'        => 'config',
-            'GLOBALTYPE'    => 'global',
+            'ACCOUNT'         => 'account',
+            'CALRESOURCE'     => 'calresource',
+            'COS'             => 'cos',
+            'DL'              => 'dl',
+            'GROUP'           => 'group',
+            'DOMAIN'          => 'domain',
+            'SERVER'          => 'server',
+            'ALWAYSONCLUSTER' => 'alwaysoncluster',
+            'UCSERVICE'       => 'ucservice',
+            'XMPPCOMPONENT'   => 'xmppcomponent',
+            'ZIMLET'          => 'zimlet',
+            'CONFIG'          => 'config',
+            'GLOBALTYPE'      => 'global',
         ];
         foreach ($values as $enum => $value)
         {
