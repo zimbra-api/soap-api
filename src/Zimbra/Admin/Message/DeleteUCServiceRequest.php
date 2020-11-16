cd4f4353934ff3dcececb3b31581a58c6c7023d8
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-11-16 12:05:42 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/DeleteUCServiceRequest.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlElement, XmlRoot};
+use Zimbra\Soap\Request;
+
+/**
+ * DeleteUCServiceRequest class
+ * Delete a UC service
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="DeleteUCServiceRequest")
+ */
+class DeleteUCServiceRequest extends Request
+{
+    /**
+     * Zimbra ID
+     * @Accessor(getter="getId", setter="setId")
+     * @SerializedName("id")
+     * @Type("string")
+     * @XmlElement(cdata=false)
+     */
+    private $id;
+
+    /**
+     * Constructor method for DeleteUCServiceRequest
+     * @param  string $id Zimbra ID
+     * @return self
+     */
+    public function __construct($id)
+    {
+        $this->setId($id);
+    }
+
+    /**
+     * Gets zimbra id
+     *
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets zimbra id
+     *
+     * @param  string $id
+     * @return self
+     */
+    public function setId($id): self
+    {
+        $this->id = trim($id);
+        return $this;
+    }
+
+    protected function internalInit()
+    {
+        $this->envelope = new DeleteUCServiceEnvelope(
+            NULL,
+            new DeleteUCServiceBody($this)
+        );
+    }
+}
