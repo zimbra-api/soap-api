cfe724f0aa6a66f538441a8bca547c2a65e12f4d
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-11-16 10:04:37 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/DelegateAuthRequest.php
@@ -0,0 +1,117 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlElement, XmlRoot};
+use Zimbra\Soap\Request;
+use Zimbra\Struct\AccountSelector as Account;
+
+/**
+ * DelegateAuthRequest request class
+ * Used to request a new auth token that is valid for the specified account.
+ * The id of the auth token will be the id of the target account, and the requesting admin's id will be stored in
+ * the auth token for auditing purposes.
+ *
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="DelegateAuthRequest")
+ */
+class DelegateAuthRequest extends Request
+{
+    /**
+     * Details of target account
+     * @Accessor(getter="getAccount", setter="setAccount")
+     * @SerializedName("account")
+     * @Type("Zimbra\Struct\AccountSelector")
+     * @XmlElement
+     */
+    private $account;
+
+    /**
+     * Lifetime in seconds of the newly-created authtoken. defaults to 1 hour.
+     * Can't be longer then zimbraAuthTokenLifetime.
+     * @Accessor(getter="getDuration", setter="setDuration")
+     * @SerializedName("duration")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $duration;
+
+    /**
+     * Constructor method for DelegateAuthRequest
+     * @param  Account $account
+     * @param  integer $duration
+     * @return self
+     */
+    public function __construct(Account $account, $duration = NULL)
+    {
+        $this->setAccount($account);
+        if (NULL !== $duration) {
+            $this->setDuration($duration);
+        }
+    }
+
+    /**
+     * Sets the account.
+     *
+     * @return Account
+     */
+    public function getAccount(): Account
+    {
+        return $this->account;
+    }
+
+    /**
+     * Sets the account.
+     *
+     * @param  Account $account
+     * @return self
+     */
+    public function setAccount(Account $account): self
+    {
+        $this->account = $account;
+        return $this;
+    }
+
+    /**
+     * Gets duration
+     *
+     * @return integer
+     */
+    public function getDuration(): ?integer
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Sets duration
+     *
+     * @param  integer $id
+     * @return self
+     */
+    public function setDuration($duration): self
+    {
+        $this->duration = (int) $duration;
+        return $this;
+    }
+
+    protected function internalInit()
+    {
+        $this->envelope = new DelegateAuthEnvelope(
+            NULL,
+            new DelegateAuthBody($this)
+        );
+    }
+}
---
+++ src/Zimbra/Admin/Message/DelegateAuthResponse.php
@@ -0,0 +1,103 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlElement, XmlRoot};
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * DelegateAuthResponse class
+ *
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="DelegateAuthResponse")
+ */
+class DelegateAuthResponse implements ResponseInterface
+{
+
+    /**
+     * Auth token
+     * @Accessor(getter="getAuthToken", setter="setAuthToken")
+     * @SerializedName("authToken")
+     * @Type("string")
+     * @XmlElement(cdata=false)
+     */
+    private $authToken;
+
+    /**
+     * Life time for the authorization
+     * @Accessor(getter="getLifetime", setter="setLifetime")
+     * @SerializedName("lifetime")
+     * @Type("int")
+     * @XmlElement(cdata=false)
+     */
+    private $lifetime;
+
+    /**
+     * Constructor method for DelegateAuthResponse
+     * @param string $authToken
+     * @param int    $lifetime
+     * @return self
+     */
+    public function __construct($authToken, $lifetime)
+    {
+        $this->setAuthToken($authToken)
+             ->setLifetime($lifetime);
+    }
+
+    /**
+     * Gets auth token
+     *
+     * @return string
+     */
+    public function getAuthToken(): string
+    {
+        return $this->authToken;
+    }
+
+    /**
+     * Sets auth token
+     *
+     * @param  string $authToken
+     * @return self
+     */
+    public function setAuthToken($authToken): self
+    {
+        $this->authToken = trim($authToken);
+        return $this;
+    }
+
+    /**
+     * Gets lifetime
+     *
+     * @return int
+     */
+    public function getLifetime(): int
+    {
+        return $this->lifetime;
+    }
+
+    /**
+     * Sets lifetime
+     *
+     * @param  int $lifetime
+     * @return self
+     */
+    public function setLifetime($lifetime): self
+    {
+        $this->lifetime = (int) $lifetime;
+        return $this;
+    }
+}
