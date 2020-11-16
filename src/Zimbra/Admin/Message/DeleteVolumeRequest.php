83cb98abc71c0647a0a51e400cbb628120b0cd3a
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-11-16 12:10:39 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/DeleteVolumeRequest.php
@@ -0,0 +1,78 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlRoot};
+use Zimbra\Soap\Request;
+
+/**
+ * DeleteVolumeRequest class
+ * Delete a volume
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="DeleteVolumeRequest")
+ */
+class DeleteVolumeRequest extends Request
+{
+    /**
+     * Volume ID
+     * @Accessor(getter="getId", setter="setId")
+     * @SerializedName("id")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $id;
+
+    /**
+     * Constructor method for DeleteVolumeRequest
+     * @param  int $id
+     * @return self
+     */
+    public function __construct($id)
+    {
+        $this->setId($id);
+    }
+
+    /**
+     * Gets Volume id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets Volume id
+     *
+     * @param  int $id
+     * @return self
+     */
+    public function setId($id): self
+    {
+        $this->id = (int) $id;
+        return $this;
+    }
+
+    protected function internalInit()
+    {
+        $this->envelope = new DeleteVolumeEnvelope(
+            NULL,
+            new DeleteVolumeBody($this)
+        );
+    }
+}
