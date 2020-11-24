ab54e907ec691637b478335e35999dcbcad4bbaa
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-11-24 11:58:04 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/GetAllConfigRequest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\XmlRoot;
+use Zimbra\Soap\Request;
+
+/**
+ * GetAllConfigRequest class
+ * Gets the aggregate quota usage for all domains on the server.
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @XmlRoot(name="GetAllConfigRequest")
+ */
+class GetAllConfigRequest extends Request
+{
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof GetAllConfigEnvelope)) {
+            $this->envelope = new GetAllConfigEnvelope(
+                new GetAllConfigBody($this)
+            );
+        }
+    }
+}
