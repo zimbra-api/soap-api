de6431da566279d3181fd8dae0e5ad082b45127d
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-11-26 14:31:05 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/GetAllServersResponse.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyserver and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlList, XmlRoot};
+use Zimbra\Admin\Struct\ServerInfo;
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * GetAllServersResponse class
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="GetAllServersResponse")
+ */
+class GetAllServersResponse implements ResponseInterface
+{
+    /**
+     * Information about servers
+     * 
+     * @Accessor(getter="getServerList", setter="setServerList")
+     * @SerializedName("server")
+     * @Type("array<Zimbra\Admin\Struct\ServerInfo>")
+     * @XmlList(inline = true, entry = "server")
+     */
+    private $serverList;
+
+    /**
+     * Constructor method for GetAllServersResponse
+     * @param array $serverList
+     * @return self
+     */
+    public function __construct(array $serverList = [])
+    {
+        $this->setServerList($serverList);
+    }
+
+    /**
+     * Add a server information
+     *
+     * @param  ServerInfo $server
+     * @return self
+     */
+    public function addServer(ServerInfo $server): self
+    {
+        $this->serverList[] = $server;
+        return $this;
+    }
+
+    /**
+     * Sets server informations
+     *
+     * @param  array $serverList
+     * @return self
+     */
+    public function setServerList(array $serverList): self
+    {
+        $this->serverList = [];
+        foreach ($serverList as $server) {
+            if ($server instanceof ServerInfo) {
+                $this->serverList[] = $server;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Gets server informations
+     *
+     * @return array
+     */
+    public function getServerList(): array
+    {
+        return $this->serverList;
+    }
+}
