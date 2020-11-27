5d74dc421bab9f5f14d1ab21496c2576aeedf48c
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-11-27 17:18:00 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/GetConfigRequest.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlElement, XmlRoot};
+use Zimbra\Admin\Struct\Attr;
+use Zimbra\Soap\Request;
+
+/**
+ * GetConfigRequest class
+ * Get Config request
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="GetConfigRequest")
+ */
+class GetConfigRequest extends Request
+{
+    /**
+     * Attribute
+     * @Accessor(getter="getAttr", setter="setAttr")
+     * @SerializedName("a")
+     * @Type("Zimbra\Admin\Struct\Attr")
+     * @XmlElement
+     */
+    private $attr;
+
+    /**
+     * Constructor method for GetConfigRequest
+     * 
+     * @param  Attr $attr
+     * @return self
+     */
+    public function __construct(Attr $attr = NULL)
+    {
+        if ($attr instanceof Attr) {
+            $this->setAttr($attr);
+        }
+    }
+
+    /**
+     * Gets the attr.
+     *
+     * @return Attr
+     */
+    public function getAttr(): ?Attr
+    {
+        return $this->attr;
+    }
+
+    /**
+     * Sets the attr.
+     *
+     * @param  Attr $attr
+     * @return self
+     */
+    public function setAttr(Attr $attr): self
+    {
+        $this->attr = $attr;
+        return $this;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof GetConfigEnvelope)) {
+            $this->envelope = new GetConfigEnvelope(
+                new GetConfigBody($this)
+            );
+        }
+    }
+}
