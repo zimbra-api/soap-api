f4de8f965d94f36b366165c64b9cce602672fc0e
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-11-29 21:02:19 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/GetDelegatedAdminConstraintsResponse.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlList, XmlRoot};
+use Zimbra\Admin\Struct\ConstraintAttr;
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * GetDelegatedAdminConstraintsResponse class
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="GetDelegatedAdminConstraintsResponse")
+ */
+class GetDelegatedAdminConstraintsResponse implements ResponseInterface
+{
+    /**
+     * Constraint attributes
+     * 
+     * @Accessor(getter="getAttrs", setter="setAttrs")
+     * @SerializedName("a")
+     * @Type("array<Zimbra\Admin\Struct\ConstraintAttr>")
+     * @XmlList(inline = true, entry = "a")
+     */
+    private $attrs;
+
+    /**
+     * Constructor method for GetDelegatedAdminConstraintsResponse
+     * @param array $attrs
+     * @return self
+     */
+    public function __construct(array $attrs = [])
+    {
+        $this->setAttrs($attrs);
+    }
+
+    /**
+     * Add a constraint attribute
+     *
+     * @param  ConstraintAttr $attr
+     * @return self
+     */
+    public function addAttr(ConstraintAttr $attr): self
+    {
+        $this->attrs[] = $attr;
+        return $this;
+    }
+
+    /**
+     * Sets constraint attributes
+     *
+     * @param  array $attrs
+     * @return self
+     */
+    public function setAttrs(array $attrs): self
+    {
+        $this->attrs = [];
+        foreach ($attrs as $attr) {
+            if ($attr instanceof ConstraintAttr) {
+                $this->attrs[] = $attr;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Gets constraint attributes
+     *
+     * @return array
+     */
+    public function getAttrs(): array
+    {
+        return $this->attrs;
+    }
+}
