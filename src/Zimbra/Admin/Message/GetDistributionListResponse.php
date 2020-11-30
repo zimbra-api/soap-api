bb8e4782c108d8ef70ef69b84cfa2f9b35759d3b
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-11-30 08:48:00 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/GetDistributionListResponse.php
@@ -0,0 +1,142 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlElement, XmlRoot};
+use Zimbra\Admin\Struct\DistributionListInfo;
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * GetDistributionListResponse class
+ *
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="GetDistributionListResponse")
+ */
+class GetDistributionListResponse implements ResponseInterface
+{
+    /**
+     * 1 (true) if more mailboxes left to return
+     * Only present if the list of members is given
+     * @Accessor(getter="isMore", setter="setMore")
+     * @SerializedName("more")
+     * @Type("bool")
+     * @XmlAttribute
+     */
+    private $more;
+
+    /**
+     * Total number of members (not affected by limit/total)
+    * Only present if the list of members is given
+     * @Accessor(getter="getTotal", setter="setTotal")
+     * @SerializedName("total")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $total;
+
+    /**
+     * Information about distribution list
+     * @Accessor(getter="getDl", setter="setDl")
+     * @SerializedName("dl")
+     * @Type("Zimbra\Admin\Struct\DistributionListInfo")
+     * @XmlElement
+     */
+    private $dl;
+
+    /**
+     * Constructor method for GetDistributionListResponse
+     * @param DistributionListInfo $dl
+     * @return self
+     */
+    public function __construct(DistributionListInfo $dl = NULL, ?bool $more = NULL, ?int $total = NULL)
+    {
+        if ($dl instanceof DistributionListInfo) {
+            $this->setDl($dl);
+        }
+        if (NULL !== $more) {
+            $this->setMore($more);
+        }
+        if (NULL !== $total) {
+            $this->setTotal($total);
+        }
+    }
+
+    /**
+     * Gets more
+     *
+     * @return bool
+     */
+    public function isMore(): ?bool
+    {
+        return $this->more;
+    }
+
+    /**
+     * Sets more
+     *
+     * @param  bool $more
+     * @return self
+     */
+    public function setMore(bool $more): self
+    {
+        $this->more = $more;
+        return $this;
+    }
+
+    /**
+     * Gets total
+     *
+     * @return int
+     */
+    public function getTotal(): ?int
+    {
+        return $this->total;
+    }
+
+    /**
+     * Sets total
+     *
+     * @param  int $total
+     * @return self
+     */
+    public function setTotal(int $total): self
+    {
+        $this->total = $total;
+        return $this;
+    }
+
+    /**
+     * Gets the dl.
+     *
+     * @return DistributionListInfo
+     */
+    public function getDl(): ?DistributionListInfo
+    {
+        return $this->dl;
+    }
+
+    /**
+     * Sets the dl.
+     *
+     * @param  DistributionListInfo $dl
+     * @return self
+     */
+    public function setDl(DistributionListInfo $dl): self
+    {
+        $this->dl = $dl;
+        return $this;
+    }
+}
---
+++ src/Zimbra/Admin/Tests/Message/GetDistributionListTest.php
@@ -0,0 +1,145 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Admin\Tests\Message;
+
+use Zimbra\Admin\Message\GetDistributionListBody;
+use Zimbra\Admin\Message\GetDistributionListEnvelope;
+use Zimbra\Admin\Message\GetDistributionListRequest;
+use Zimbra\Admin\Message\GetDistributionListResponse;
+
+use Zimbra\Admin\Struct\DistributionListInfo;
+use Zimbra\Admin\Struct\DistributionListSelector;
+use Zimbra\Admin\Struct\GranteeInfo;
+use Zimbra\Enum\DistributionListBy as DLBy;
+use Zimbra\Enum\GranteeType;
+
+use Zimbra\Struct\Tests\ZimbraStructTestCase;
+
+/**
+ * Testcase class for GetDistributionListTest.
+ */
+class GetDistributionListTest extends ZimbraStructTestCase
+{
+    public function testGetDistributionList()
+    {
+        $id = $this->faker->uuid;
+        $name = $this->faker->word;
+        $value = $this->faker->word;
+        $member = $this->faker->word;
+        $attrs = $this->faker->word;
+        $limit = mt_rand(1, 10);
+        $offset = mt_rand(1, 10);
+        $total = mt_rand(1, 10);
+
+        $owner = new GranteeInfo(
+            $id, $name, GranteeType::ALL()
+        );
+        $dlSel = new DistributionListSelector(DLBy::NAME(), $value);
+        $dlInfo = new DistributionListInfo($name, $id, [$member], [], [$owner], TRUE);
+
+        $request = new GetDistributionListRequest($dlSel, $limit, $offset, FALSE, $attrs);
+        $this->assertSame($dlSel, $request->getDl());
+        $this->assertSame($limit, $request->getLimit());
+        $this->assertSame($offset, $request->getOffset());
+        $this->assertFalse($request->isSortAscending());
+        $this->assertSame($attrs, $request->getAttrs());
+
+        $request = new GetDistributionListRequest();
+        $request->setDl($dlSel)
+            ->setLimit($limit)
+            ->setOffset($offset)
+            ->setSortAscending(TRUE)
+            ->setAttrs($attrs);
+        $this->assertSame($dlSel, $request->getDl());
+        $this->assertSame($limit, $request->getLimit());
+        $this->assertSame($offset, $request->getOffset());
+        $this->assertTrue($request->isSortAscending());
+        $this->assertSame($attrs, $request->getAttrs());
+
+        $response = new GetDistributionListResponse($dlInfo, FALSE, $total);
+        $this->assertSame($dlInfo, $response->getDl());
+        $this->assertFalse($response->isMore());
+        $this->assertSame($total, $response->getTotal());
+        $response = new GetDistributionListResponse();
+        $response->setDl($dlInfo)
+            ->setMore(TRUE)
+            ->setTotal($total);
+        $this->assertSame($dlInfo, $response->getDl());
+        $this->assertTrue($response->isMore());
+        $this->assertSame($total, $response->getTotal());
+
+        $body = new GetDistributionListBody($request, $response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+        $body = new GetDistributionListBody();
+        $body->setRequest($request)
+             ->setResponse($response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+
+        $envelope = new GetDistributionListEnvelope($body);
+        $this->assertSame($body, $envelope->getBody());
+        $envelope = new GetDistributionListEnvelope();
+        $envelope->setBody($body);
+        $this->assertSame($body, $envelope->getBody());
+
+        $xml = '<?xml version="1.0"?>' . "\n"
+            . '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:zimbraAdmin">'
+                . '<soap:Body>'
+                    . '<urn:GetDistributionListRequest attrs="' . $attrs . '" limit="' . $limit . '" offset="' . $offset . '" sortAscending="true">'
+                        . '<dl by="' . DLBy::NAME() . '">' . $value . '</dl>'
+                    . '</urn:GetDistributionListRequest>'
+                    . '<urn:GetDistributionListResponse more="true" total="' . $total . '">'
+                        . '<dl name="' . $name . '" id="' . $id . '" dynamic="true">'
+                            . '<dlm>' . $member . '</dlm>'
+                            . '<owners>'
+                                . '<owner id="' . $id . '" name="' . $name . '" type="' . GranteeType::ALL() . '" />'
+                            . '</owners>'
+                        . '</dl>'
+                    . '</urn:GetDistributionListResponse>'
+                . '</soap:Body>'
+            . '</soap:Envelope>';
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($envelope, 'xml'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($xml, GetDistributionListEnvelope::class, 'xml'));
+
+        $json = json_encode([
+            'Body' => [
+                'GetDistributionListRequest' => [
+                    'attrs' => $attrs,
+                    'limit' => $limit,
+                    'offset' => $offset,
+                    'sortAscending' => TRUE,
+                    'dl' => [
+                        'by' => (string) DLBy::NAME(),
+                        '_content' => $value,
+                    ],
+                    '_jsns' => 'urn:zimbraAdmin',
+                ],
+                'GetDistributionListResponse' => [
+                    'more' => TRUE,
+                    'total' => $total,
+                    'dl' => [
+                        'name' => $name,
+                        'id' => $id,
+                        'dynamic' => TRUE,
+                        'dlm' => [
+                            ['_content' => $member],
+                        ],
+                        'owners' => [
+                            'owner' => [
+                                [
+                                    'id' => $id,
+                                    'name' => $name,
+                                    'type' => (string) GranteeType::ALL(),
+                                ],
+                            ],
+                        ],
+                    ],
+                    '_jsns' => 'urn:zimbraAdmin',
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($envelope, 'json'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($json, GetDistributionListEnvelope::class, 'json'));
+    }
+}
