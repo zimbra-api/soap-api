852309b8fba6cbbac8b5c65a13464f325c57637c
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-12-14 18:09:18 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/GetMemcachedClientConfigRequest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\XmlRoot;
+use Zimbra\Soap\Request;
+
+/**
+ * GetMemcachedClientConfigRequest request class
+ * Get MailBox Statistics
+ *
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @XmlRoot(name="GetMemcachedClientConfigRequest")
+ */
+class GetMemcachedClientConfigRequest extends Request
+{
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof GetMemcachedClientConfigEnvelope)) {
+            $this->envelope = new GetMemcachedClientConfigEnvelope(
+                new GetMemcachedClientConfigBody($this)
+            );
+        }
+    }
+}
