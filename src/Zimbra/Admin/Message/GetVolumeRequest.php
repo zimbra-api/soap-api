88d2f6c994203eb0cab7b33d0546e2473fd552f8
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-12-16 16:19:22 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/GetVolumeRequest.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlRoot};
+use Zimbra\Soap\Request;
+
+/**
+ * GetVolumeRequest class
+ * Get Volume
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="GetVolumeRequest")
+ */
+class GetVolumeRequest extends Request
+{
+    /**
+     * ID of volume
+     * @Accessor(getter="getId", setter="setId")
+     * @SerializedName("id")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $id;
+
+    /**
+     * Constructor method for GetVolumeRequest
+     *
+     * @param  int $id
+     * @return self
+     */
+    public function __construct(int $id)
+    {
+        $this->setId($id);
+    }
+
+    /**
+     * Gets volume id
+     *
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets volume id
+     *
+     * @param  int $id
+     * @return self
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof GetVolumeEnvelope)) {
+            $this->envelope = new GetVolumeEnvelope(
+                new GetVolumeBody($this)
+            );
+        }
+    }
+}
