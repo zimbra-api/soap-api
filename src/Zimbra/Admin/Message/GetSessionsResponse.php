d963455b79d6f87815b71e04a6018e0840d50888
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-12-16 10:58:10 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/GetSessionsResponse.php
@@ -0,0 +1,157 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlList, XmlRoot};
+use Zimbra\Admin\Struct\SimpleSessionInfo;
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * GetSessionsResponse class
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="GetSessionsResponse")
+ */
+class GetSessionsResponse implements ResponseInterface
+{
+    /**
+     * 1 (true) if more sessions left to return
+     * @Accessor(getter="getMore", setter="setMore")
+     * @SerializedName("more")
+     * @Type("bool")
+     * @XmlAttribute
+     */
+    private $more;
+
+    /**
+     * Total number of accounts that matched search (not affected by limit/offset)
+     * @Accessor(getter="getTotal", setter="setTotal")
+     * @SerializedName("total")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $total;
+
+    /**
+     * Session information
+     * @Accessor(getter="getSessions", setter="setSessions")
+     * @SerializedName("s")
+     * @Type("array<Zimbra\Admin\Struct\SimpleSessionInfo>")
+     * @XmlList(inline = true, entry = "s")
+     */
+    private $sessions;
+
+    /**
+     * Constructor method for GetSessionsResponse
+     * 
+     * @param bool $more
+     * @param int $total
+     * @param array $sessions
+     * @return self
+     */
+    public function __construct(
+    	bool $more,
+        int $total,
+        array $sessions = [])
+    {
+        $this->setMore($more)
+        	 ->setTotal($total)
+        	 ->setSessions($sessions);
+    }
+
+    /**
+     * Gets more
+     *
+     * @return bool
+     */
+    public function getMore(): bool
+    {
+        return $this->more;
+    }
+
+    /**
+     * Sets more
+     *
+     * @param  bool $more
+     * @return self
+     */
+    public function setMore(bool $more): self
+    {
+        $this->more = $more;
+        return $this;
+    }
+
+    /**
+     * Gets total
+     *
+     * @return int
+     */
+    public function getTotal(): int
+    {
+        return $this->total;
+    }
+
+    /**
+     * Sets total
+     *
+     * @param  int $total
+     * @return self
+     */
+    public function setTotal(int $total): self
+    {
+        $this->total = $total;
+        return $this;
+    }
+
+    /**
+     * Add session information
+     *
+     * @param  SimpleSessionInfo $session
+     * @return self
+     */
+    public function addSession(SimpleSessionInfo $session): self
+    {
+        $this->sessions[] = $session;
+        return $this;
+    }
+
+    /**
+     * Sets session information
+     *
+     * @param array $sessions
+     * @return self
+     */
+    public function setSessions(array $sessions): self
+    {
+        $this->sessions = [];
+        foreach ($sessions as $session) {
+            if ($session instanceof SimpleSessionInfo) {
+                $this->sessions[] = $session;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Gets session information
+     *
+     * @return array
+     */
+    public function getSessions(): array
+    {
+        return $this->sessions;
+    }
+}
---
+++ src/Zimbra/Admin/Struct/SimpleSessionInfo.php
@@ -0,0 +1,208 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Struct;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlAttributeMap, XmlElement, XmlRoot};
+
+/**
+ * SimpleSessionInfo struct class
+ *
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Struct
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="session")
+ */
+class SimpleSessionInfo
+{
+    /**
+     * Account ID
+     * @Accessor(getter="getZimbraId", setter="setZimbraId")
+     * @SerializedName("zid")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $zimbraId;
+
+    /**
+     * Account name
+     * @Accessor(getter="getName", setter="setName")
+     * @SerializedName("name")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $name;
+
+    /**
+     * Session ID
+     * @Accessor(getter="getSessionId", setter="setSessionId")
+     * @SerializedName("sid")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $sessionId;
+
+    /**
+     * Creation date
+     * @Accessor(getter="getCreatedDate", setter="setCreatedDate")
+     * @SerializedName("cd")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $createdDate;
+
+    /**
+     * Last accessed date
+     * @Accessor(getter="getLastAccessedDate", setter="setLastAccessedDate")
+     * @SerializedName("ld")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $lastAccessedDate;
+
+    /**
+     * Constructor method for SimpleSessionInfo
+     *
+     * @param string $zimbraId
+     * @param string $name
+     * @param string $sessionId
+     * @param int $createdDate
+     * @param int $lastAccessedDate
+     * @return self
+     */
+    public function __construct(
+        string $zimbraId,
+        string $name,
+        string $sessionId,
+        int $createdDate,
+        int $lastAccessedDate
+    )
+    {
+        $this->setZimbraId($zimbraId)
+             ->setName($name)
+             ->setSessionId($sessionId)
+             ->setCreatedDate($createdDate)
+             ->setLastAccessedDate($lastAccessedDate);
+
+    }
+
+    /**
+     * Gets session ID
+     *
+     * @return string
+     */
+    public function getSessionId(): string
+    {
+        return $this->sessionId;
+    }
+
+    /**
+     * Sets session ID
+     *
+     * @param  string $sessionId
+     * @return self
+     */
+    public function setSessionId(string $sessionId): self
+    {
+        $this->sessionId = $sessionId;
+        return $this;
+    }
+
+    /**
+     * Gets the createdDate
+     *
+     * @return int
+     */
+    public function getCreatedDate(): int
+    {
+        return $this->createdDate;
+    }
+
+    /**
+     * Sets the createdDate
+     *
+     * @param  int $createdDate
+     * @return self
+     */
+    public function setCreatedDate(int $createdDate): self
+    {
+        $this->createdDate = $createdDate;
+        return $this;
+    }
+
+    /**
+     * Gets the lastAccessedDate
+     *
+     * @return int
+     */
+    public function getLastAccessedDate(): int
+    {
+        return $this->lastAccessedDate;
+    }
+
+    /**
+     * Sets the lastAccessedDate
+     *
+     * @param  int $lastAccessedDate
+     * @return self
+     */
+    public function setLastAccessedDate(int $lastAccessedDate): self
+    {
+        $this->lastAccessedDate = $lastAccessedDate;
+        return $this;
+    }
+
+    /**
+     * Gets the zimbraId
+     *
+     * @return string
+     */
+    public function getZimbraId(): string
+    {
+        return $this->zimbraId;
+    }
+
+    /**
+     * Sets the zimbraId
+     *
+     * @param  string $stdname
+     * @return self
+     */
+    public function setZimbraId(string $zimbraId): self
+    {
+        $this->zimbraId = $zimbraId;
+        return $this;
+    }
+
+    /**
+     * Gets the name
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the name
+     *
+     * @param  string $name
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
