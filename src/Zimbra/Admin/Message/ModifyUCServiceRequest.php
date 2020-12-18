fad234a2f87776949e42c1f0aa52b98ad0ebb21c
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-12-18 15:36:46 +0700

    Add ModifyUCService Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/ModifyUCServiceRequest.php
@@ -0,0 +1,93 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlElement, XmlRoot};
+use Zimbra\Admin\Struct\{AdminAttrs, AdminAttrsImplTrait};
+use Zimbra\Soap\Request;
+
+/**
+ * ModifyUCServiceRequest class
+ * Modify attributes for a server 
+ * Notes:
+ * - an empty attribute value removes the specified attr
+ * - this request is by default proxied to the referenced server
+ *
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="ModifyUCServiceRequest")
+ */
+class ModifyUCServiceRequest extends Request implements AdminAttrs
+{
+    use AdminAttrsImplTrait;
+
+    /**
+     * Zimbra ID
+     * @Accessor(getter="getId", setter="setId")
+     * @SerializedName("id")
+     * @Type("string")
+     * @XmlElement(cdata = false)
+     */
+    private $id;
+
+    /**
+     * Constructor method for ModifyUCServiceRequest
+     * 
+     * @param string $id
+     * @param array  $attrs
+     * @return self
+     */
+    public function __construct(string $id, array $attrs = [])
+    {
+        $this->setId($id)
+             ->setAttrs($attrs);
+    }
+
+    /**
+     * Gets id
+     *
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets id
+     *
+     * @param  string $id
+     * @return self
+     */
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof ModifyUCServiceEnvelope)) {
+            $this->envelope = new ModifyUCServiceEnvelope(
+                new ModifyUCServiceBody($this)
+            );
+        }
+    }
+}
