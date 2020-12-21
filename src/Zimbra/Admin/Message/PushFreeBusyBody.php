169c42cc765f573999cc2e3b24a6b2cd09cc2372
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-12-21 09:45:45 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/PushFreeBusyBody.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlElement, XmlRoot};
+use Zimbra\Soap\{Body, RequestInterface, ResponseInterface};
+
+/**
+ * PushFreeBusyBody class
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="Body")
+ */
+class PushFreeBusyBody extends Body
+{
+    /**
+     * @Accessor(getter="getRequest", setter="setRequest")
+     * @SerializedName("PushFreeBusyRequest")
+     * @Type("Zimbra\Admin\Message\PushFreeBusyRequest")
+     * @XmlElement(namespace="urn:zimbraAdmin")
+     */
+    private $request;
+
+    /**
+     * @Accessor(getter="getResponse", setter="setResponse")
+     * @SerializedName("PushFreeBusyResponse")
+     * @Type("Zimbra\Admin\Message\PushFreeBusyResponse")
+     * @XmlElement(namespace="urn:zimbraAdmin")
+     */
+    private $response;
+
+    /**
+     * Constructor method for PushFreeBusyBody
+     * @return self
+     */
+    public function __construct(?PushFreeBusyRequest $request = NULL, ?PushFreeBusyResponse $response = NULL)
+    {
+        parent::__construct($request, $response);
+    }
+
+    public function setRequest(RequestInterface $request): self
+    {
+        if ($request instanceof PushFreeBusyRequest) {
+            $this->request = $request;
+        }
+        return $this;
+    }
+
+    public function getRequest(): ?RequestInterface
+    {
+        return $this->request;
+    }
+
+    public function setResponse(ResponseInterface $response): self
+    {
+        if ($response instanceof PushFreeBusyResponse) {
+            $this->response = $response;
+        }
+        return $this;
+    }
+
+    public function getResponse(): ?ResponseInterface
+    {
+        return $this->response;
+    }
+}
