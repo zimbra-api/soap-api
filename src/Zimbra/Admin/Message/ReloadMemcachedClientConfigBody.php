d926c5fbd2dd6e40f524c09412bdc01465d390a6
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-12-21 17:45:52 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/ReloadMemcachedClientConfigBody.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlElement, XmlRoot};
+use Zimbra\Soap\{Body, RequestInterface, ResponseInterface};
+
+/**
+ * ReloadMemcachedClientConfigBody class
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="Body")
+ */
+class ReloadMemcachedClientConfigBody extends Body
+{
+    /**
+     * @Accessor(getter="getRequest", setter="setRequest")
+     * @SerializedName("ReloadMemcachedClientConfigRequest")
+     * @Type("Zimbra\Admin\Message\ReloadMemcachedClientConfigRequest")
+     * @XmlElement(namespace="urn:zimbraAdmin")
+     */
+    private $request;
+
+    /**
+     * @Accessor(getter="getResponse", setter="setResponse")
+     * @SerializedName("ReloadMemcachedClientConfigResponse")
+     * @Type("Zimbra\Admin\Message\ReloadMemcachedClientConfigResponse")
+     * @XmlElement(namespace="urn:zimbraAdmin")
+     */
+    private $response;
+
+    /**
+     * Constructor method for ReloadMemcachedClientConfigBody
+     * @return self
+     */
+    public function __construct(
+        ?ReloadMemcachedClientConfigRequest $request = NULL, ?ReloadMemcachedClientConfigResponse $response = NULL
+    )
+    {
+        parent::__construct($request, $response);
+    }
+
+    public function setRequest(RequestInterface $request): self
+    {
+        if ($request instanceof ReloadMemcachedClientConfigRequest) {
+            $this->request = $request;
+        }
+        return $this;
+    }
+
+    public function getRequest(): ?RequestInterface
+    {
+        return $this->request;
+    }
+
+    public function setResponse(ResponseInterface $response): self
+    {
+        if ($response instanceof ReloadMemcachedClientConfigResponse) {
+            $this->response = $response;
+        }
+        return $this;
+    }
+
+    public function getResponse(): ?ResponseInterface
+    {
+        return $this->response;
+    }
+}
