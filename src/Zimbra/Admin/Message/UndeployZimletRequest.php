c83284bcb21ebbaa166b384f85737647fdf23595
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2020-12-23 11:03:00 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Admin/Message/UndeployZimletRequest.php
@@ -0,0 +1,120 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlRoot};
+use Zimbra\Soap\Request;
+
+/**
+ * UndeployZimletRequest class
+ * Undeploy Zimlet
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="UndeployZimletRequest")
+ */
+class UndeployZimletRequest extends Request
+{
+    /**
+     * Zimlet name
+     * @Accessor(getter="getName", setter="setName")
+     * @SerializedName("name")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $name;
+
+    /**
+     * Action
+     * @Accessor(getter="getAction", setter="setAction")
+     * @SerializedName("action")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $action;
+
+    /**
+     * Constructor method for UndeployZimletRequest
+     *
+     * @param  string $name
+     * @param  string $action
+     * @return self
+     */
+    public function __construct(string $name, ?string $action = NULL)
+    {
+        $this->setName($name);
+        if (NULL !== $action) {
+            $this->setAction($action);
+        }
+    }
+
+    /**
+     * Gets zimbra name
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets zimbra name
+     *
+     * @param  string $name
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Gets action
+     *
+     * @return string
+     */
+    public function getAction(): ?string
+    {
+        return $this->action;
+    }
+
+    /**
+     * Sets action
+     *
+     * @param  string $action
+     * @return self
+     */
+    public function setAction(string $action): self
+    {
+        $this->action = $action;
+        return $this;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof UndeployZimletEnvelope)) {
+            $this->envelope = new UndeployZimletEnvelope(
+                new UndeployZimletBody($this)
+            );
+        }
+    }
+}
