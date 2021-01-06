94f3054b10c19024c83d5f18da05335680401b4e
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2021-01-06 17:18:01 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Zimbra/Account/Struct/HABMember.php
@@ -0,0 +1,102 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Account\Struct;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlAttribute, XmlElement};
+
+/**
+ * HABMember struct class
+ *
+ * @package    Zimbra
+ * @subpackage Account
+ * @category   Struct
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+abstract class HABMember
+{
+    /**
+     * HAB Member name - an email address (user@domain)
+     * @Accessor(getter="getName", setter="setName")
+     * @SerializedName("name")
+     * @Type("string")
+     * @XmlElement(cdata = false)
+     */
+    private $name;
+
+    /**
+     * seniorityIndex of the HAB group member
+     * @Accessor(getter="getSeniorityIndex", setter="setSeniorityIndex")
+     * @SerializedName("seniorityIndex")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $seniorityIndex;
+
+    /**
+     * Constructor method for HABMember
+     * 
+     * @param  string $name
+     * @param  int $seniorityIndex
+     * @return self
+     */
+    public function __construct(string $name, ?int $seniorityIndex = NULL)
+    {
+        $this->setName($name);
+        if (NULL !== $seniorityIndex) {
+            $this->setSeniorityIndex($seniorityIndex);
+        }
+    }
+
+    /**
+     * Gets name
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets name
+     *
+     * @param  string $name
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Gets seniorityIndex
+     *
+     * @return int
+     */
+    public function getSeniorityIndex(): ?int
+    {
+        return $this->seniorityIndex;
+    }
+
+    /**
+     * Sets seniorityIndex
+     *
+     * @param  int $seniorityIndex
+     * @return self
+     */
+    public function setSeniorityIndex(int $seniorityIndex): self
+    {
+        $this->seniorityIndex = $seniorityIndex;
+        return $this;
+    }
+}
---
+++ src/Zimbra/Account/Tests/Struct/HABGroupMemberTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Account\Tests\Struct;
+
+use JMS\Serializer\Annotation\XmlRoot;
+use Zimbra\Account\Struct\HABGroupMember;
+use Zimbra\Struct\NamedValue;
+use Zimbra\Struct\Tests\ZimbraStructTestCase;
+
+/**
+ * Testcase class for HABGroupMember.
+ */
+class HABGroupMemberTest extends ZimbraStructTestCase
+{
+    public function testHABGroupMember()
+    {
+        $name = $this->faker->email;
+        $seniorityIndex = mt_rand(1, 100);
+
+        $key1 = $this->faker->word;
+        $value1 = $this->faker->word;
+        $key2 = $this->faker->word;
+        $value2 = $this->faker->word;
+
+        $attr1 = new NamedValue($key1, $value1);
+        $attr2 = new NamedValue($key2, $value2);
+
+        $groupMember = new HABGroupMember($name, $seniorityIndex, [$attr1]);
+        $this->assertSame([$attr1], $groupMember->getAttrs());
+        $groupMember->setAttrs([$attr1])->addAttr($attr2);
+        $this->assertSame([$attr1, $attr2], $groupMember->getAttrs());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<groupMember seniorityIndex="$seniorityIndex">
+    <name>$name</name>
+    <attr name="$key1">$value1</attr>
+    <attr name="$key2">$value2</attr>
+</groupMember>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($groupMember, 'xml'));
+        $this->assertEquals($groupMember, $this->serializer->deserialize($xml, HABGroupMember::class, 'xml'));
+
+        $json = json_encode([
+            'name' => [
+                '_content' => $name,
+            ],
+            'seniorityIndex' => $seniorityIndex,
+            'attr' => [
+                [
+                    'name' => $key1,
+                    '_content' => $value1,
+                ],
+                [
+                    'name' => $key2,
+                    '_content' => $value2,
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($groupMember, 'json'));
+        $this->assertEquals($groupMember, $this->serializer->deserialize($json, HABGroupMember::class, 'json'));
+    }
+}
