62759ffb6d967f8442fc90e98c326adb940e35e4
Nguyen Van Nguyen <nguyennv@localhost.localdomain>
2021-01-19 17:29:44 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv@localhost.localdomain>

---
+++ src/Zimbra/Account/Message/GetPrefsRequest.php
@@ -0,0 +1,105 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Account\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlList, XmlRoot};
+use Zimbra\Account\Struct\Pref;
+use Zimbra\Soap\Request;
+
+/**
+ * GetPrefsRequest class
+ * Get preferences for the authenticated account
+ * If no <pref> elements are provided, all known prefs are returned in the response.
+ * If <pref> elements are provided, only those prefs are returned in the response.
+ * 
+ * @package    Zimbra
+ * @subpackage Account
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2020 by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="GetPrefsRequest")
+ */
+class GetPrefsRequest extends Request
+{
+    /**
+     * If any of these are specified then only get these preferences
+     * @Accessor(getter="getPrefs", setter="setPrefs")
+     * @SerializedName("pref")
+     * @Type("array<Zimbra\Account\Struct\Pref>")
+     * @XmlList(inline = true, entry = "pref")
+     */
+    private $prefs;
+
+    /**
+     * Constructor method for GetPrefsRequest
+     *
+     * @param  array $prefs
+     * @return self
+     */
+    public function __construct(array $prefs = [])
+    {
+        $this->setPrefs($prefs);
+    }
+
+    /**
+     * Add a pref
+     *
+     * @param  Pref $pref
+     * @return self
+     */
+    public function addPref(Pref $pref): self
+    {
+        $this->prefs[] = $pref;
+        return $this;
+    }
+
+    /**
+     * Set prefs
+     *
+     * @param  array $prefs
+     * @return self
+     */
+    public function setPrefs(array $prefs): self
+    {
+        $this->prefs = [];
+        foreach ($prefs as $pref) {
+            if ($pref instanceof Pref) {
+                $this->prefs[] = $pref;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Gets prefs
+     *
+     * @return array
+     */
+    public function getPrefs(): array
+    {
+        return $this->prefs;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof GetPrefsEnvelope)) {
+            $this->envelope = new GetPrefsEnvelope(
+                new GetPrefsBody($this)
+            );
+        }
+    }
+}
---
+++ src/Zimbra/Account/Message/GetPrefsResponse.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Account\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlList, XmlRoot};
+use Zimbra\Account\Struct\Pref;
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * GetPrefsResponse class
+ * 
+ * @package    Zimbra
+ * @subpackage Account
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2020 by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="GetPrefsResponse")
+ */
+class GetPrefsResponse implements ResponseInterface
+{
+    /**
+     * Preferences
+     * @Accessor(getter="getPrefs", setter="setPrefs")
+     * @SerializedName("pref")
+     * @Type("array<Zimbra\Account\Struct\Pref>")
+     * @XmlList(inline = true, entry = "pref")
+     */
+    private $prefs;
+
+    /**
+     * Constructor method for GetPrefsResponse
+     * 
+     * @param  array $prefs
+     * @return self
+     */
+    public function __construct(array $prefs = [])
+    {
+        $this->setPrefs($prefs);
+    }
+
+    /**
+     * Add a pref
+     *
+     * @param  Pref $pref
+     * @return self
+     */
+    public function addPref(Pref $pref): self
+    {
+        $this->prefs[] = $pref;
+        return $this;
+    }
+
+    /**
+     * Set prefs
+     *
+     * @param  array $prefs
+     * @return self
+     */
+    public function setPrefs(array $prefs): self
+    {
+        $this->prefs = [];
+        foreach ($prefs as $pref) {
+            if ($pref instanceof Pref) {
+                $this->prefs[] = $pref;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Gets prefs
+     *
+     * @return array
+     */
+    public function getPrefs(): array
+    {
+        return $this->prefs;
+    }
+}
