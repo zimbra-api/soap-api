d005efbd1290a30fcdfb2f71422dc5e373a15b04
Nguyen Van Nguyen <nguyennv@localhost.localdomain>
2021-01-20 13:39:20 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv@localhost.localdomain>

---
+++ src/Zimbra/Account/Tests/Struct/CalendarResourceInfoTest.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Account\Tests\Struct;
+
+use Zimbra\Account\Struct\CalendarResourceInfo;
+use Zimbra\Struct\KeyValuePair;
+use Zimbra\Struct\Tests\ZimbraStructTestCase;
+
+/**
+ * Testcase class for CalendarResourceInfo.
+ */
+class CalendarResourceInfoTest extends ZimbraStructTestCase
+{
+    public function testCalendarResourceInfo()
+    {
+        $name = $this->faker->word;
+        $id = $this->faker->uuid;
+
+        $key1 = $this->faker->word;
+        $value1 = $this->faker->word;
+        $key2 = $this->faker->word;
+        $value2 = $this->faker->word;
+
+        $attr1 = new KeyValuePair($key1, $value1);
+        $attr2 = new KeyValuePair($key2, $value2);
+
+        $info = new CalendarResourceInfo($name, $id, [$attr1]);
+        $this->assertSame($name, $info->getName());
+        $this->assertSame($id, $info->getId());
+        $this->assertSame([$attr1], $info->getKeyValuePairs());
+
+        $info = new CalendarResourceInfo('', '');
+        $info->setName($name)
+             ->setId($id)
+             ->setKeyValuePairs([$attr1])
+             ->addKeyValuePair($attr2);
+        $this->assertSame($name, $info->getName());
+        $this->assertSame($id, $info->getId());
+        $this->assertSame([$attr1, $attr2], $info->getKeyValuePairs());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<calresource name="$name" id="$id">
+    <a n="$key1">$value1</a>
+    <a n="$key2">$value2</a>
+</calresource>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($info, 'xml'));
+        $this->assertEquals($info, $this->serializer->deserialize($xml, CalendarResourceInfo::class, 'xml'));
+
+        $json = json_encode([
+            'name' => $name,
+            'id' => $id,
+            'a' => [
+                [
+                    'n' => $key1,
+                    '_content' => $value1,
+                ],
+                [
+                    'n' => $key2,
+                    '_content' => $value2,
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($info, 'json'));
+        $this->assertEquals($info, $this->serializer->deserialize($json, CalendarResourceInfo::class, 'json'));
+    }
+}
