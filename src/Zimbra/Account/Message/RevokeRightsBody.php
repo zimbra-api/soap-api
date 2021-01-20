d5bee66684d8e654af79805522cc7e08f683f592
Nguyen Van Nguyen <nguyennv@localhost.localdomain>
2021-01-20 12:25:23 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv@localhost.localdomain>

---
+++ src/Zimbra/Account/Message/RevokeRightsBody.php
@@ -0,0 +1,82 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Account\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlElement, XmlRoot};
+use Zimbra\Soap\{Body, RequestInterface, ResponseInterface};
+
+/**
+ * RevokeRightsBody class
+ * 
+ * @package    Zimbra
+ * @subpackage Account
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="Body")
+ */
+class RevokeRightsBody extends Body
+{
+    /**
+     * @Accessor(getter="getRequest", setter="setRequest")
+     * @SerializedName("RevokeRightsRequest")
+     * @Type("Zimbra\Account\Message\RevokeRightsRequest")
+     * @XmlElement(namespace="urn:zimbraAccount")
+     */
+    private $request;
+
+    /**
+     * @Accessor(getter="getResponse", setter="setResponse")
+     * @SerializedName("RevokeRightsResponse")
+     * @Type("Zimbra\Account\Message\RevokeRightsResponse")
+     * @XmlElement(namespace="urn:zimbraAccount")
+     */
+    private $response;
+
+    /**
+     * Constructor method for RevokeRightsBody
+     *
+     * @return self
+     */
+    public function __construct(
+        ?RevokeRightsRequest $request = NULL, ?RevokeRightsResponse $response = NULL
+    )
+    {
+        parent::__construct($request, $response);
+    }
+
+    public function setRequest(RequestInterface $request): self
+    {
+        if ($request instanceof RevokeRightsRequest) {
+            $this->request = $request;
+        }
+        return $this;
+    }
+
+    public function getRequest(): ?RequestInterface
+    {
+        return $this->request;
+    }
+
+    public function setResponse(ResponseInterface $response): self
+    {
+        if ($response instanceof RevokeRightsResponse) {
+            $this->response = $response;
+        }
+        return $this;
+    }
+
+    public function getResponse(): ?ResponseInterface
+    {
+        return $this->response;
+    }
+}
