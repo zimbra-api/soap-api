52f84a79d558c128a2c38bc6ce4f4ffb38ed91ec
Nguyen Van Nguyen <nguyennv@localhost.localdomain>
2021-01-20 11:51:43 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv@localhost.localdomain>

---
+++ src/Zimbra/Account/Message/ResetPasswordRequest.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Account\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlElement, XmlRoot};
+use Zimbra\Soap\Request;
+
+/**
+ * ResetPasswordRequest class
+ * Reset Password
+ *
+ * @package    Zimbra
+ * @subpackage Account
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="ResetPasswordRequest")
+ */
+class ResetPasswordRequest extends Request
+{
+    /**
+     * New Password to assign
+     * @Accessor(getter="getPassword", setter="setPassword")
+     * @SerializedName("password")
+     * @Type("string")
+     * @XmlElement
+     */
+    private $password;
+
+    /**
+     * Constructor method for ResetPasswordRequest
+     * 
+     * @param string $password
+     * @return self
+     */
+    public function __construct(string $password)
+    {
+        $this->setPassword($password);
+    }
+
+    /**
+     * Gets the password.
+     *
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * Sets the password.
+     *
+     * @param  string $password
+     * @return self
+     */
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof ResetPasswordEnvelope)) {
+            $this->envelope = new ResetPasswordEnvelope(
+                new ResetPasswordBody($this)
+            );
+        }
+    }
+}
---
+++ src/Zimbra/Account/Tests/Message/ResetPasswordTest.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Account\Tests\Message;
+
+use Zimbra\Account\Message\{ResetPasswordEnvelope, ResetPasswordBody, ResetPasswordRequest, ResetPasswordResponse};
+use Zimbra\Struct\Tests\ZimbraStructTestCase;
+/**
+ * Testcase class for ResetPassword.
+ */
+class ResetPasswordTest extends ZimbraStructTestCase
+{
+    public function testResetPassword()
+    {
+        $password = $this->faker->password;
+
+        $request = new ResetPasswordRequest($password);
+        $this->assertSame($password, $request->getPassword());
+        $request = new ResetPasswordRequest('');
+        $request->setPassword($password);
+        $this->assertSame($password, $request->getPassword());
+
+        $response = new ResetPasswordResponse();
+
+        $body = new ResetPasswordBody($request, $response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+        $body = new ResetPasswordBody();
+        $body->setRequest($request)
+            ->setResponse($response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+
+        $envelope = new ResetPasswordEnvelope($body);
+        $this->assertSame($body, $envelope->getBody());
+
+        $envelope = new ResetPasswordEnvelope();
+        $envelope->setBody($body);
+        $this->assertSame($body, $envelope->getBody());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:zimbraAccount">
+    <soap:Body>
+        <urn:ResetPasswordRequest>
+            <password>$password</password>
+        </urn:ResetPasswordRequest>
+        <urn:ResetPasswordResponse />
+    </soap:Body>
+</soap:Envelope>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($envelope, 'xml'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($xml, ResetPasswordEnvelope::class, 'xml'));
+
+        $json = json_encode([
+            'Body' => [
+                'ResetPasswordRequest' => [
+                    'password' => [
+                        '_content' => $password,
+                    ],
+                    '_jsns' => 'urn:zimbraAccount',
+                ],
+                'ResetPasswordResponse' => [
+                    '_jsns' => 'urn:zimbraAccount',
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($envelope, 'json'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($json, ResetPasswordEnvelope::class, 'json'));
+    }
+}
