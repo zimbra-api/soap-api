a502176b3d5b69910de517c74b550d3961750da1
Nguyen Van Nguyen <nguyennv@localhost.localdomain>
2021-01-27 15:26:36 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv@localhost.localdomain>

--- src/Zimbra/Mail/Struct/InviteComponent.php
+++ src/Zimbra/Mail/Struct/InviteComponent.php
@@ -13,6 +13,12 @@ namespace Zimbra\Mail\Struct;
 use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlElement, XmlList, XmlRoot};
 
 use Zimbra\Struct\InviteComponentInterface;
+use Zimbra\Struct\GeoInfoInterface;
+use Zimbra\Struct\CalOrganizerInterface;
+use Zimbra\Struct\RecurrenceInfoInterface;
+use Zimbra\Struct\ExceptionRecurIdInfoInterface;
+use Zimbra\Struct\DtTimeInfoInterface;
+use Zimbra\Struct\DurationInfoInterface;
 
 /**
  * InviteComponent struct class
@@ -173,40 +179,476 @@ class InviteComponent extends InviteComponentCommon implements InviteComponentIn
     private $duration;
 
     /**
-     * Constructor method for InviteComponent
-     * 
-     * @param int $index
-     * @param bool $negative
-     * @param string $header
+     * Constructor method
+     *
+     * @param string $method
+     * @param int $componentNum
+     * @param bool $rsvp
      * @return self
      */
-    public function __construct(?int $index = NULL, ?bool $negative = NULL, ?string $header = NULL)
+    public function __construct(
+        ?string $method = NULL,
+        ?int $componentNum = NULL,
+        ?bool $rsvp = NULL
+    )
     {
-    	parent::__construct($index, $negative);
-        if (NULL !== $header) {
-            $this->setHeader($header);
-        }
+        parent::__construct($method, $componentNum, $rsvp);
     }
 
     /**
-     * Gets header
+     * Gets categories
+     *
+     * @return array
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /**
+     * Sets categories
+     *
+     * @param  array $categories
+     * @return self
+     */
+    public function setCategories(array $categories)
+    {
+        $this->categories = [];
+        foreach ($categories as $category) {
+            $this->addCategory($category);
+        }
+        return $this;
+    }
+
+    /**
+     * add category
+     *
+     * @param  string $category
+     * @return self
+     */
+    public function addCategory(string $category)
+    {
+        $category = trim($category);
+        if (!in_array($category, $this->categories)) {
+            $this->categories[] = $category;
+        }
+        return $this;
+    }
+
+    /**
+     * Gets comments
+     *
+     * @return array
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    /**
+     * Sets comments
+     *
+     * @param  array $comments
+     * @return self
+     */
+    public function setComments(array $comments)
+    {
+        $this->comments = [];
+        foreach ($comments as $comment) {
+            $this->addComment($comment);
+        }
+        return $this;
+    }
+
+    /**
+     * add comment
+     *
+     * @param  string $comment
+     * @return self
+     */
+    public function addComment(string $comment)
+    {
+        $comment = trim($comment);
+        if (!in_array($comment, $this->comments)) {
+            $this->comments[] = $comment;
+        }
+        return $this;
+    }
+
+    /**
+     * Gets contacts
+     *
+     * @return array
+     */
+    public function getContacts()
+    {
+        return $this->contacts;
+    }
+
+    /**
+     * Sets contacts
+     *
+     * @param  array $contacts
+     * @return self
+     */
+    public function setContacts(array $contacts)
+    {
+        $this->contacts = [];
+        foreach ($contacts as $contact) {
+            $this->addContact($contact);
+        }
+        return $this;
+    }
+
+    /**
+     * add contact
+     *
+     * @param  string $contact
+     * @return self
+     */
+    public function addContact(string $contact)
+    {
+        $contact = trim($contact);
+        if (!in_array($contact, $this->contacts)) {
+            $this->contacts[] = $contact;
+        }
+        return $this;
+    }
+
+    /**
+     * Gets fragment
      *
      * @return string
      */
-    public function getHeader(): ?string
+    public function getFragment(): ?string
     {
-        return $this->header;
+        return $this->fragment;
     }
 
     /**
-     * Sets header
+     * Sets fragment
      *
-     * @param  string $header
+     * @param  string $fragment
      * @return self
      */
-    public function setHeader(string $header)
+    public function setFragment(string $fragment): self
     {
-        $this->header = $header;
+        $this->fragment = $fragment;
+        return $this;
+    }
+
+    /**
+     * Gets description
+     *
+     * @return string
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * Sets description
+     *
+     * @param  string $description
+     * @return self
+     */
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * Gets htmlDescription
+     *
+     * @return string
+     */
+    public function getHtmlDescription(): ?string
+    {
+        return $this->htmlDescription;
+    }
+
+    /**
+     * Sets htmlDescription
+     *
+     * @param  string $htmlDescription
+     * @return self
+     */
+    public function setHtmlDescription(string $htmlDescription): self
+    {
+        $this->htmlDescription = $htmlDescription;
+        return $this;
+    }
+
+    /**
+     * Gets geo
+     *
+     * @return GeoInfoInterface
+     */
+    public function getGeo(): ?GeoInfoInterface
+    {
+        return $this->geo;
+    }
+
+    /**
+     * Sets geo
+     *
+     * @param  GeoInfoInterface $geo
+     * @return self
+     */
+    public function setGeo(GeoInfoInterface $geo): self
+    {
+        $this->geo = $geo;
+        return $this;
+    }
+
+    /**
+     * Sets attendees
+     *
+     * @param  array $attendees
+     * @return self
+     */
+    public function setAttendees(array $attendees): self
+    {
+        $this->attendees = [];
+        foreach ($attendees as $attendee) {
+            if ($attendee instanceof CalendarAttendeeInterface) {
+                $this->attendees[] = $attendee;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Gets attendees
+     *
+     * @return array
+     */
+    public function getAttendees(): array
+    {
+        return $this->attendees;
+    }
+
+    /**
+     * Add attendee
+     *
+     * @param  CalendarAttendeeInterface $attendee
+     * @return self
+     */
+    public function addAttendee(CalendarAttendeeInterface $attendee): self
+    {
+        $this->attendees[] = $attendee;
+        return $this;
+    }
+
+    /**
+     * Sets alarms
+     *
+     * @param  array $alarms
+     * @return self
+     */
+    public function setAlarms(array $alarms): self
+    {
+        $this->alarms = [];
+        foreach ($alarms as $alarm) {
+            if ($alarm instanceof AlarmInfoInterface) {
+                $this->alarms[] = $alarm;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Gets alarms
+     *
+     * @return array
+     */
+    public function getAlarms(): array
+    {
+        return $this->alarms;
+    }
+
+    /**
+     * Add alarm
+     *
+     * @param  AlarmInfoInterface $alarm
+     * @return self
+     */
+    public function addAlarm(AlarmInfoInterface $alarm): self
+    {
+        $this->alarms[] = $alarm;
+        return $this;
+    }
+
+    /**
+     * Sets xProps
+     *
+     * @param  array $xProps
+     * @return self
+     */
+    public function setXProps(array $xProps): self
+    {
+        $this->xProps = [];
+        foreach ($xProps as $xProp) {
+            if ($xProp instanceof XPropInterface) {
+                $this->xProps[] = $xProp;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Gets xProps
+     *
+     * @return array
+     */
+    public function getXProps(): array
+    {
+        return $this->xProps;
+    }
+
+    /**
+     * Add xProp
+     *
+     * @param  XPropInterface $xProp
+     * @return self
+     */
+    public function addXProp(XPropInterface $xProp): self
+    {
+        $this->xProps[] = $xProp;
+        return $this;
+    }
+
+    /**
+     * Gets organizer
+     *
+     * @return CalOrganizerInterface
+     */
+    public function getOrganizer(): ?CalOrganizerInterface
+    {
+        return $this->organizer;
+    }
+
+    /**
+     * Sets organizer
+     *
+     * @param  CalOrganizerInterface $organizer
+     * @return self
+     */
+    public function setOrganizer(CalOrganizerInterface $organizer): self
+    {
+        $this->organizer = $organizer;
+        return $this;
+    }
+
+    /**
+     * Gets recurrence
+     *
+     * @return RecurrenceInfoInterface
+     */
+    public function getRecurrence(): ?RecurrenceInfoInterface
+    {
+        return $this->recurrence;
+    }
+
+    /**
+     * Sets recurrence
+     *
+     * @param  RecurrenceInfoInterface $recurrence
+     * @return self
+     */
+    public function setRecurrence(RecurrenceInfoInterface $recurrence): self
+    {
+        $this->recurrence = $recurrence;
+        return $this;
+    }
+
+    /**
+     * Gets exceptionId
+     *
+     * @return ExceptionRecurIdInfoInterface
+     */
+    public function getExceptionId(): ?ExceptionRecurIdInfoInterface
+    {
+        return $this->exceptionId;
+    }
+
+    /**
+     * Sets exceptionId
+     *
+     * @param  ExceptionRecurIdInfoInterface $exceptionId
+     * @return self
+     */
+    public function setExceptionId(ExceptionRecurIdInfoInterface $exceptionId): self
+    {
+        $this->exceptionId = $exceptionId;
+        return $this;
+    }
+
+    /**
+     * Gets dtStart
+     *
+     * @return DtTimeInfoInterface
+     */
+    public function getDtStart(): ?DtTimeInfoInterface
+    {
+        return $this->dtStart;
+    }
+
+    /**
+     * Sets dtStart
+     *
+     * @param  DtTimeInfoInterface $dtStart
+     * @return self
+     */
+    public function setDtStart(DtTimeInfoInterface $dtStart): self
+    {
+        $this->dtStart = $dtStart;
+        return $this;
+    }
+
+    /**
+     * Gets dtEnd
+     *
+     * @return DtTimeInfoInterface
+     */
+    public function getDtEnd(): ?DtTimeInfoInterface
+    {
+        return $this->dtEnd;
+    }
+
+    /**
+     * Sets dtEnd
+     *
+     * @param  DtTimeInfoInterface $dtEnd
+     * @return self
+     */
+    public function setDtEnd(DtTimeInfoInterface $dtEnd): self
+    {
+        $this->dtEnd = $dtEnd;
+        return $this;
+    }
+
+    /**
+     * Gets duration
+     *
+     * @return DurationInfoInterface
+     */
+    public function getDuration(): ?DurationInfoInterface
+    {
+        return $this->duration;
+    }
+
+    /**
+     * Sets duration
+     *
+     * @param  DurationInfoInterface $duration
+     * @return self
+     */
+    public function setDuration(DurationInfoInterface $duration): self
+    {
+        $this->duration = $duration;
         return $this;
     }
 }
