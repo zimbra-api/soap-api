c3cf69bb1ed14ff034d435883b16364e94662931
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2021-01-29 09:25:11 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+(static function () {
+    if (!is_file($autoloadFile = __DIR__ . '/../vendor/autoload.php')) {
+        throw new RuntimeException('Did not find vendor/autoload.php. Did you run "composer install --dev"?');
+    }
+
+    $loader = require $autoloadFile;
+    $loader->add('Zimbra\Tests', __DIR__);
+
+    AnnotationRegistry::registerLoader('class_exists');
+})();
