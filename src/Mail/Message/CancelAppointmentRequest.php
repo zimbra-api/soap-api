05e8fbdd31d3e7c2f6f1da09910a0bddc3b71b9d
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2021-02-02 10:59:48 +0700

    WIP Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/CancelAppointmentRequest.php
@@ -0,0 +1,310 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlElement, XmlRoot};
+use Zimbra\Mail\Struct\CalTZInfo;
+use Zimbra\Mail\Struct\InstanceRecurIdInfo;
+use Zimbra\Mail\Struct\Msg;
+use Zimbra\Soap\Request;
+
+/**
+ * CancelAppointmentRequest class
+ * Cancel appointment
+ * NOTE: If canceling an exception, the original instance (ie the one the exception was "excepting") WILL NOT be
+ * restored when you cancel this exception.
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="CancelAppointmentRequest")
+ */
+class CancelAppointmentRequest extends Request
+{
+    /**
+     * ID of default invite
+     * @Accessor(getter="getId", setter="setId")
+     * @SerializedName("id")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $id;
+
+    /**
+     * Component number of default invite
+     * @Accessor(getter="getComponentNum", setter="setComponentNum")
+     * @SerializedName("comp")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $componentNum;
+
+    /**
+     * Modified sequence
+     * @Accessor(getter="getModifiedSequence", setter="setModifiedSequence")
+     * @SerializedName("ms")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $modifiedSequence;
+
+    /**
+     * Revision
+     * @Accessor(getter="getRevision", setter="setRevision")
+     * @SerializedName("rev")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $revision;
+
+    /**
+     * Instance recurrence ID information
+     * @Accessor(getter="getInstance", setter="setInstance")
+     * @SerializedName("inst")
+     * @Type("Zimbra\Mail\Struct\InstanceRecurIdInfo")
+     * @XmlElement
+     */
+    private $instance;
+
+    /**
+     * Definition for TZID referenced by DATETIME in instance
+     * @Accessor(getter="getTimezone", setter="setTimezone")
+     * @SerializedName("tz")
+     * @Type("Zimbra\Mail\Struct\CalTZInfo")
+     * @XmlElement
+     */
+    private $timezone;
+
+    /**
+     * Message
+     * @Accessor(getter="getMsg", setter="setMsg")
+     * @SerializedName("m")
+     * @Type("Zimbra\Mail\Struct\Msg")
+     * @XmlElement
+     */
+    private $msg;
+
+    /**
+     * Constructor method for CancelAppointmentRequest
+     *
+     * @param  string $id
+     * @param  int $componentNum
+     * @param  int $modifiedSequence
+     * @param  int $revision
+     * @param  InstanceRecurIdInfo $= 
+     * @param  CalTZInfo $timezone
+     * @param  Msg $msg
+     * @return self
+     */
+    public function __construct(
+        ?string $id = NULL,
+        ?int $componentNum = NULL,
+        ?int $modifiedSequence = NULL,
+        ?int $revision = NULL,
+        ?InstanceRecurIdInfo $instance = NULL,
+        ?CalTZInfo $timezone = NULL,
+        ?Msg $msg = NULL
+    )
+    {
+        if (NULL !== $id) {
+            $this->setId($id);
+        }
+        if (NULL !== $componentNum) {
+            $this->setComponentNum($componentNum);
+        }
+        if (NULL !== $modifiedSequence) {
+            $this->setModifiedSequence($modifiedSequence);
+        }
+        if (NULL !== $revision) {
+            $this->setRevision($revision);
+        }
+        if ($instance instanceof InstanceRecurIdInfo) {
+            $this->setInstance($instance);
+        }
+        if ($timezone instanceof CalTZInfo) {
+            $this->setTimezone($timezone);
+        }
+        if ($msg instanceof Msg) {
+            $this->setMsg($msg);
+        }
+    }
+
+    /**
+     * Sets id
+     *
+     * @param  string $id
+     * @return self
+     */
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Gets id
+     *
+     * @return string
+     */
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets componentNum
+     *
+     * @param  int $componentNum
+     * @return self
+     */
+    public function setComponentNum(int $componentNum): self
+    {
+        $this->componentNum = $componentNum;
+        return $this;
+    }
+
+    /**
+     * Gets componentNum
+     *
+     * @return int
+     */
+    public function getComponentNum(): ?int
+    {
+        return $this->componentNum;
+    }
+
+    /**
+     * Sets modifiedSequence
+     *
+     * @param  int $modifiedSequence
+     * @return self
+     */
+    public function setModifiedSequence(int $modifiedSequence): self
+    {
+        $this->modifiedSequence = $modifiedSequence;
+        return $this;
+    }
+
+    /**
+     * Gets modifiedSequence
+     *
+     * @return int
+     */
+    public function getModifiedSequence(): ?int
+    {
+        return $this->modifiedSequence;
+    }
+
+    /**
+     * Sets revision
+     *
+     * @param  int $revision
+     * @return self
+     */
+    public function setRevision(int $revision): self
+    {
+        $this->revision = $revision;
+        return $this;
+    }
+
+    /**
+     * Gets revision
+     *
+     * @return int
+     */
+    public function getRevision(): ?int
+    {
+        return $this->revision;
+    }
+
+    /**
+     * Sets instance
+     *
+     * @param  InstanceRecurIdInfo $instance
+     * @return self
+     */
+    public function setInstance(InstanceRecurIdInfo $instance): self
+    {
+        $this->instance = $instance;
+        return $this;
+    }
+
+    /**
+     * Gets instance
+     *
+     * @return InstanceRecurIdInfo
+     */
+    public function getInstance(): ?InstanceRecurIdInfo
+    {
+        return $this->instance;
+    }
+
+    /**
+     * Sets timezone
+     *
+     * @param  CalTZInfo $timezone
+     * @return self
+     */
+    public function setTimezone(CalTZInfo $timezone): self
+    {
+        $this->timezone = $timezone;
+        return $this;
+    }
+
+    /**
+     * Gets timezone
+     *
+     * @return CalTZInfo
+     */
+    public function getTimezone(): ?CalTZInfo
+    {
+        return $this->timezone;
+    }
+
+    /**
+     * Sets msg
+     *
+     * @param  Msg $msg
+     * @return self
+     */
+    public function setMsg(Msg $msg): self
+    {
+        $this->msg = $msg;
+        return $this;
+    }
+
+    /**
+     * Gets msg
+     *
+     * @return Msg
+     */
+    public function getMsg(): ?Msg
+    {
+        return $this->msg;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof CancelAppointmentEnvelope)) {
+            $this->envelope = new CancelAppointmentEnvelope(
+                new CancelAppointmentBody($this)
+            );
+        }
+    }
+}
---
+++ tests/Mail/Message/CancelAppointmentTest.php
@@ -0,0 +1,214 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Message;
+
+use Zimbra\Enum\AddressType;
+use Zimbra\Enum\ReplyType;
+
+use Zimbra\Mail\Message\CancelAppointmentEnvelope;
+use Zimbra\Mail\Message\CancelAppointmentBody;
+use Zimbra\Mail\Message\CancelAppointmentRequest;
+use Zimbra\Mail\Message\CancelAppointmentResponse;
+
+use Zimbra\Mail\Struct\AttachmentsInfo;
+use Zimbra\Mail\Struct\CalTZInfo;
+use Zimbra\Mail\Struct\EmailAddrInfo;
+use Zimbra\Mail\Struct\Header;
+use Zimbra\Mail\Struct\InstanceRecurIdInfo;
+use Zimbra\Mail\Struct\InvitationInfo;
+use Zimbra\Mail\Struct\MimePartInfo;
+use Zimbra\Mail\Struct\Msg;
+
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for CancelAppointment.
+ */
+class CancelAppointmentTest extends ZimbraTestCase
+{
+    public function testCancelAppointment()
+    {
+        $id = $this->faker->word;
+        $componentNum = $this->faker->randomNumber;
+        $modifiedSequence = $this->faker->randomNumber;
+        $revision = $this->faker->randomNumber;
+        $range = $this->faker->randomElement(['THISANDFUTURE', 'THISANDPRIOR']);
+        $dateTime = $this->faker->date;
+        $tz = $this->faker->timezone;
+        $tzStdOffset = $this->faker->randomNumber;
+        $tzDayOffset = $this->faker->randomNumber;
+
+        $origId = $this->faker->uuid;
+        $replyType = ReplyType::REPLIED();
+        $identityId = $this->faker->uuid;
+        $subject = $this->faker->text;
+        $inReplyTo = $this->faker->uuid;
+        $folderId = $this->faker->uuid;
+        $flags = $this->faker->word;
+        $content = $this->faker->text;
+        $fragment = $this->faker->text;
+        $contentType = $this->faker->word;
+        $contentId = $this->faker->uuid;
+        $name = $this->faker->name;
+        $value = $this->faker->word;
+        $method = $this->faker->word;
+        $address = $this->faker->email;
+        $personal = $this->faker->word;
+
+        $header = new Header($name, $value);
+        $mimePart = new MimePartInfo($contentType, $content, $contentId);
+        $attachments = new AttachmentsInfo($id);
+        $invite = new InvitationInfo($method, $componentNum, TRUE);
+        $emailAddress = new EmailAddrInfo($address, AddressType::FROM(), $personal);
+
+        $timezone = new CalTZInfo($id, $tzStdOffset, $tzDayOffset);
+        $msg = new Msg(
+            $id, $origId, $replyType, $identityId, $subject, [$header], $inReplyTo, $folderId, $flags, $content, $mimePart, $attachments, $invite, [$emailAddress], [$timezone], $fragment
+        );
+        $instance = new InstanceRecurIdInfo($range, $dateTime, $tz);
+
+        $request = new CancelAppointmentRequest(
+            $id, $componentNum, $modifiedSequence, $revision, $instance, $timezone, $msg
+        );
+        $this->assertSame($id, $request->getId());
+        $this->assertSame($componentNum, $request->getComponentNum());
+        $this->assertSame($modifiedSequence, $request->getModifiedSequence());
+        $this->assertSame($revision, $request->getRevision());
+        $this->assertSame($instance, $request->getInstance());
+        $this->assertSame($timezone, $request->getTimezone());
+        $this->assertSame($msg, $request->getMsg());
+        $request = new CancelAppointmentRequest();
+        $request->setId($id)
+            ->setComponentNum($componentNum)
+            ->setModifiedSequence($modifiedSequence)
+            ->setRevision($revision)
+            ->setInstance($instance)
+            ->setTimezone($timezone)
+            ->setMsg($msg);
+        $this->assertSame($id, $request->getId());
+        $this->assertSame($componentNum, $request->getComponentNum());
+        $this->assertSame($modifiedSequence, $request->getModifiedSequence());
+        $this->assertSame($revision, $request->getRevision());
+        $this->assertSame($instance, $request->getInstance());
+        $this->assertSame($timezone, $request->getTimezone());
+        $this->assertSame($msg, $request->getMsg());
+
+        $response = new CancelAppointmentResponse();
+
+        $body = new CancelAppointmentBody($request, $response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+        $body = new CancelAppointmentBody();
+        $body->setRequest($request)
+            ->setResponse($response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+
+        $envelope = new CancelAppointmentEnvelope($body);
+        $this->assertSame($body, $envelope->getBody());
+        $envelope = new CancelAppointmentEnvelope();
+        $envelope->setBody($body);
+        $this->assertSame($body, $envelope->getBody());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:zimbraMail">
+    <soap:Body>
+        <urn:CancelAppointmentRequest id="$id" comp="$componentNum" ms="$modifiedSequence" rev="$revision">
+            <inst range="$range" d="$dateTime" tz="$tz" />
+            <tz id="$id" stdoff="$tzStdOffset" dayoff="$tzDayOffset" />
+            <m aid="$id" origid="$origId" rt="r" idnt="$identityId" su="$subject" irt="$inReplyTo" l="$folderId" f="$flags">
+                <header name="$name">$value</header>
+                <content>$content</content>
+                <mp ct="$contentType" content="$content" ci="$contentId" />
+                <attach aid="$id" />
+                <inv method="$method" compNum="$componentNum" rsvp="true" />
+                <e a="$address" t="f" p="$personal" />
+                <tz id="$id" stdoff="$tzStdOffset" dayoff="$tzDayOffset" />
+                <fr>$fragment</fr>
+            </m>
+        </urn:CancelAppointmentRequest>
+        <urn:CancelAppointmentResponse />
+    </soap:Body>
+</soap:Envelope>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($envelope, 'xml'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($xml, CancelAppointmentEnvelope::class, 'xml'));
+
+        $json = json_encode([
+            'Body' => [
+                'CancelAppointmentRequest' => [
+                    'id' => $id,
+                    'comp' => $componentNum,
+                    'ms' => $modifiedSequence,
+                    'rev' => $revision,
+                    'inst' => [
+                        'range' => $range,
+                        'd' => $dateTime,
+                        'tz' => $tz,
+                    ],
+                    'tz' => [
+                        'id' => $id,
+                        'stdoff' => $tzStdOffset,
+                        'dayoff' => $tzDayOffset,
+                    ],
+                    'm' => [
+                        'aid' => $id,
+                        'origid' => $origId,
+                        'rt' => 'r',
+                        'idnt' => $identityId,
+                        'su' => $subject,
+                        'irt' => $inReplyTo,
+                        'l' => $folderId,
+                        'f' => $flags,
+                        'header' => [
+                            [
+                                'name' => $name,
+                                '_content' => $value,
+                            ],
+                        ],
+                        'content' => [
+                            '_content' => $content,
+                        ],
+                        'mp' => [
+                            'ct' => $contentType,
+                            'content' => $content,
+                            'ci' => $contentId,
+                        ],
+                        'attach' => [
+                            'aid' => $id,
+                        ],
+                        'inv' => [
+                            'method' => $method,
+                            'compNum' => $componentNum,
+                            'rsvp' => TRUE,
+                        ],
+                        'e' => [
+                            [
+                                'a' => $address,
+                                't' => 'f',
+                                'p' => $personal,
+                            ],
+                        ],
+                        'tz' => [
+                            [
+                                'id' => $id,
+                                'stdoff' => $tzStdOffset,
+                                'dayoff' => $tzDayOffset,
+                            ],
+                        ],
+                        'fr' => [
+                            '_content' => $fragment,
+                        ],
+                    ],
+                    '_jsns' => 'urn:zimbraMail',
+                ],
+                'CancelAppointmentResponse' => [
+                    '_jsns' => 'urn:zimbraMail',
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($envelope, 'json'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($json, CancelAppointmentEnvelope::class, 'json'));
+    }
+}
