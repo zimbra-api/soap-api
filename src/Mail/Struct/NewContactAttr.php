f92ec8b2b060e5a0fdcd284eb1880e1e8551a675
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2021-02-04 12:26:28 +0700

    WIP Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Struct/NewContactAttr.php
@@ -0,0 +1,214 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Struct;
+
+use JMS\Serializer\Annotation\{Accessor, AccessType, SerializedName, Type, XmlAttribute, XmlValue, XmlRoot};
+
+/**
+ * NewContactAttr class
+ * New contact attribute
+ *
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Struct
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @AccessType("public_method")
+ * @XmlRoot(name="attr")
+ */
+class NewContactAttr
+{
+    /**
+     * Attribute name
+     * @Accessor(getter="getName", setter="setName")
+     * @SerializedName("n")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $name;
+
+    /**
+     * Upload ID
+     * @Accessor(getter="getAttachId", setter="setAttachId")
+     * @SerializedName("aid")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $attachId;
+
+    /**
+     * Item ID.  Used in combination with subpart-name
+     * @Accessor(getter="getId", setter="setId")
+     * @SerializedName("id")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $id;
+
+    /**
+     * Subpart Name
+     * @Accessor(getter="getPart", setter="setPart")
+     * @SerializedName("part")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $part;
+
+    /**
+     * Attribute data
+     * Date related attributes like "birthday" and "anniversary" SHOULD use "yyyy-MM-dd" format or,
+     * if the year isn't specified "--MM-dd" format
+     * @Accessor(getter="getValue", setter="setValue")
+     * @SerializedName("_content")
+     * @Type("string")
+     * @XmlValue(cdata = false)
+     */
+    private $value;
+
+    /**
+     * Constructor method for NewContactAttr
+     *
+     * @param  string $name
+     * @param  string $attachId
+     * @param  int $id
+     * @param  string $part
+     * @param  string $value
+     * @return self
+     */
+    public function __construct(
+        string $name, ?string $attachId = NULL, ?int $id = NULL, ?string $part = NULL, ?string $value = NULL
+    )
+    {
+        $this->setName($name);
+        if (NULL !== $attachId) {
+            $this->setAttachId($attachId);
+        }
+        if (NULL !== $id) {
+            $this->setId($id);
+        }
+        if (NULL !== $part) {
+            $this->setPart($part);
+        }
+        if (NULL !== $value) {
+            $this->setValue($value);
+        }
+    }
+
+    /**
+     * Gets name
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets name
+     *
+     * @param  string $name
+     * @return self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Gets attachId
+     *
+     * @return string
+     */
+    public function getAttachId(): ?string
+    {
+        return $this->attachId;
+    }
+
+    /**
+     * Sets attachId
+     *
+     * @param  string $attachId
+     * @return self
+     */
+    public function setAttachId(string $attachId): self
+    {
+        $this->attachId = $attachId;
+        return $this;
+    }
+
+    /**
+     * Gets id
+     *
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets id
+     *
+     * @param  int $id
+     * @return self
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Gets part
+     *
+     * @return string
+     */
+    public function getPart(): ?string
+    {
+        return $this->part;
+    }
+
+    /**
+     * Sets part
+     *
+     * @param  string $part
+     * @return self
+     */
+    public function setPart(string $part): self
+    {
+        $this->part = $part;
+        return $this;
+    }
+
+    /**
+     * Gets value
+     *
+     * @return string
+     */
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    /**
+     * Sets value
+     *
+     * @param  string $value
+     * @return self
+     */
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+        return $this;
+    }
+}
---
+++ tests/Mail/Struct/NewContactAttrTest.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Struct;
+
+use Zimbra\Mail\Struct\NewContactAttr;
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for NewContactAttr.
+ */
+class NewContactAttrTest extends ZimbraTestCase
+{
+    public function testNewContactAttr()
+    {
+        $name = $this->faker->word;
+        $attachId = $this->faker->uuid;
+        $id = $this->faker->numberBetween(1, 100);
+        $part = $this->faker->word;
+        $value = $this->faker->word;
+
+        $attr = new NewContactAttr(
+            $name, $attachId, $id, $part, $value
+        );
+        $this->assertSame($name, $attr->getName());
+        $this->assertSame($attachId, $attr->getAttachId());
+        $this->assertSame($id, $attr->getId());
+        $this->assertSame($part, $attr->getPart());
+        $this->assertSame($value, $attr->getValue());
+
+        $attr = new NewContactAttr('');
+        $attr->setName($name)
+            ->setAttachId($attachId)
+            ->setId($id)
+            ->setPart($part)
+            ->setValue($value);
+        $this->assertSame($name, $attr->getName());
+        $this->assertSame($attachId, $attr->getAttachId());
+        $this->assertSame($id, $attr->getId());
+        $this->assertSame($part, $attr->getPart());
+        $this->assertSame($value, $attr->getValue());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<attr n="$name" aid="$attachId" id="$id" part="$part">$value</attr>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($attr, 'xml'));
+        $this->assertEquals($attr, $this->serializer->deserialize($xml, NewContactAttr::class, 'xml'));
+
+        $json = json_encode([
+            'n' => $name,
+            'aid' => $attachId,
+            'id' => $id,
+            'part' => $part,
+            '_content' => $value,
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($attr, 'json'));
+        $this->assertEquals($attr, $this->serializer->deserialize($json, NewContactAttr::class, 'json'));
+    }
+}
