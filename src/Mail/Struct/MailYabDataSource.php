4e85b84a3cc70973330b549ed2c62daa69aaac50
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2021-02-08 15:43:27 +0700

    WIP Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Struct/MailYabDataSource.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Struct;
+
+use JMS\Serializer\Annotation\XmlRoot;
+use Zimbra\Struct\DataSource;
+
+/**
+ * MailYabDataSource struct class
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Struct
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ * @XmlRoot(name="yab")
+ */
+class MailYabDataSource extends MailDataSource implements DataSource
+{
+}
---
+++ tests/Mail/Struct/MailDataSourceTest.php
@@ -0,0 +1,216 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Struct;
+
+use Zimbra\Enum\ConnectionType;
+use Zimbra\Mail\Struct\MailDataSource;
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for MailDataSource.
+ */
+class MailDataSourceTest extends ZimbraTestCase
+{
+    public function testMailDataSource()
+    {
+        $id = $this->faker->uuid;
+        $name = $this->faker->name;
+        $folderId = $this->faker->word;
+        $host = $this->faker->ipv4;
+        $port = $this->faker->randomNumber;
+        $connectionType = ConnectionType::CLEAR_TEXT();
+        $username = $this->faker->email;
+        $password = $this->faker->text;
+        $pollingInterval = $this->faker->word;
+        $emailAddress = $this->faker->email;
+        $smtpHost = $this->faker->ipv4;
+        $smtpPort = $this->faker->randomNumber;
+        $smtpConnectionType = ConnectionType::CLEAR_TEXT();
+        $smtpUsername = $this->faker->email;
+        $smtpPassword = $this->faker->text;
+        $defaultSignature = $this->faker->word;
+        $forwardReplySignature = $this->faker->word;
+        $fromDisplay = $this->faker->name;
+        $replyToAddress = $this->faker->email;
+        $replyToDisplay = $this->faker->name;
+        $importClass = $this->faker->text;
+        $failingSince = $this->faker->randomNumber;
+        $lastError = $this->faker->text;
+        $refreshToken = $this->faker->text;
+        $refreshTokenUrl = $this->faker->url;
+        $attribute = $this->faker->unique()->text;
+        $attribute1 = $this->faker->unique()->text;
+        $attribute2 = $this->faker->unique()->text;
+        $attributes = [
+            $attribute1,
+            $attribute2,
+        ];
+
+        $dataSource = new MailDataSource(
+            $id, $name, $folderId, FALSE, FALSE, $host, $port, $connectionType, $username, $password, $pollingInterval, $emailAddress,
+            FALSE, $smtpHost, $smtpPort, $smtpConnectionType, FALSE, $smtpUsername, $smtpPassword,
+            FALSE, $defaultSignature, $forwardReplySignature,$fromDisplay, $replyToAddress, $replyToDisplay, $importClass,
+            $failingSince, $lastError, $refreshToken, $refreshTokenUrl, $attributes
+        );
+        $this->assertSame($id, $dataSource->getId());
+        $this->assertSame($name, $dataSource->getName());
+        $this->assertSame($folderId, $dataSource->getFolderId());
+        $this->assertFalse($dataSource->isEnabled());
+        $this->assertFalse($dataSource->isImportOnly());
+        $this->assertSame($host, $dataSource->getHost());
+        $this->assertSame($port, $dataSource->getPort());
+        $this->assertSame($connectionType, $dataSource->getConnectionType());
+        $this->assertSame($username, $dataSource->getUsername());
+        $this->assertSame($password, $dataSource->getPassword());
+        $this->assertSame($pollingInterval, $dataSource->getPollingInterval());
+        $this->assertSame($emailAddress, $dataSource->getEmailAddress());
+        $this->assertFalse($dataSource->isSmtpEnabled());
+        $this->assertSame($smtpHost, $dataSource->getSmtpHost());
+        $this->assertSame($smtpPort, $dataSource->getSmtpPort());
+        $this->assertSame($smtpConnectionType, $dataSource->getSmtpConnectionType());
+        $this->assertFalse($dataSource->isSmtpAuthRequired());
+        $this->assertSame($smtpUsername, $dataSource->getSmtpUsername());
+        $this->assertSame($smtpPassword, $dataSource->getSmtpPassword());
+        $this->assertFalse($dataSource->isUseAddressForForwardReply());
+        $this->assertSame($defaultSignature, $dataSource->getDefaultSignature());
+        $this->assertSame($forwardReplySignature, $dataSource->getForwardReplySignature());
+        $this->assertSame($fromDisplay, $dataSource->getFromDisplay());
+        $this->assertSame($replyToAddress, $dataSource->getReplyToAddress());
+        $this->assertSame($replyToDisplay, $dataSource->getReplyToDisplay());
+        $this->assertSame($importClass, $dataSource->getImportClass());
+        $this->assertSame($failingSince, $dataSource->getFailingSince());
+        $this->assertSame($lastError, $dataSource->getLastError());
+        $this->assertSame($attributes, $dataSource->getAttributes());
+        $this->assertSame($refreshToken, $dataSource->getRefreshToken());
+        $this->assertSame($refreshTokenUrl, $dataSource->getRefreshTokenUrl());
+
+        $dataSource = new MailDataSource();
+        $dataSource->setId($id)
+            ->setName($name)
+            ->setFolderId($folderId)
+            ->setEnabled(TRUE)
+            ->setImportOnly(TRUE)
+            ->setHost($host)
+            ->setPort($port)
+            ->setConnectionType($connectionType)
+            ->setUsername($username)
+            ->setPassword($password)
+            ->setPollingInterval($pollingInterval)
+            ->setEmailAddress($emailAddress)
+            ->setSmtpEnabled(TRUE)
+            ->setSmtpHost($smtpHost)
+            ->setSmtpPort($smtpPort)
+            ->setSmtpConnectionType($smtpConnectionType)
+            ->setSmtpAuthRequired(TRUE)
+            ->setSmtpUsername($smtpUsername)
+            ->setSmtpPassword($smtpPassword)
+            ->setUseAddressForForwardReply(TRUE)
+            ->setDefaultSignature($defaultSignature)
+            ->setForwardReplySignature($forwardReplySignature)
+            ->setFromDisplay($fromDisplay)
+            ->setReplyToAddress($replyToAddress)
+            ->setReplyToDisplay($replyToDisplay)
+            ->setImportClass($importClass)
+            ->setFailingSince($failingSince)
+            ->setLastError($lastError)
+            ->setRefreshToken($refreshToken)
+            ->setRefreshTokenUrl($refreshTokenUrl)
+            ->setAttributes($attributes)
+            ->addAttribute($attribute);
+        $this->assertSame($id, $dataSource->getId());
+        $this->assertSame($name, $dataSource->getName());
+        $this->assertSame($folderId, $dataSource->getFolderId());
+        $this->assertTrue($dataSource->isEnabled());
+        $this->assertTrue($dataSource->isImportOnly());
+        $this->assertSame($host, $dataSource->getHost());
+        $this->assertSame($port, $dataSource->getPort());
+        $this->assertSame($connectionType, $dataSource->getConnectionType());
+        $this->assertSame($username, $dataSource->getUsername());
+        $this->assertSame($password, $dataSource->getPassword());
+        $this->assertSame($pollingInterval, $dataSource->getPollingInterval());
+        $this->assertSame($emailAddress, $dataSource->getEmailAddress());
+        $this->assertTrue($dataSource->isSmtpEnabled());
+        $this->assertSame($smtpHost, $dataSource->getSmtpHost());
+        $this->assertSame($smtpPort, $dataSource->getSmtpPort());
+        $this->assertSame($smtpConnectionType, $dataSource->getSmtpConnectionType());
+        $this->assertTrue($dataSource->isSmtpAuthRequired());
+        $this->assertSame($smtpUsername, $dataSource->getSmtpUsername());
+        $this->assertSame($smtpPassword, $dataSource->getSmtpPassword());
+        $this->assertTrue($dataSource->isUseAddressForForwardReply());
+        $this->assertSame($defaultSignature, $dataSource->getDefaultSignature());
+        $this->assertSame($forwardReplySignature, $dataSource->getForwardReplySignature());
+        $this->assertSame($fromDisplay, $dataSource->getFromDisplay());
+        $this->assertSame($replyToAddress, $dataSource->getReplyToAddress());
+        $this->assertSame($replyToDisplay, $dataSource->getReplyToDisplay());
+        $this->assertSame($importClass, $dataSource->getImportClass());
+        $this->assertSame($failingSince, $dataSource->getFailingSince());
+        $this->assertSame($lastError, $dataSource->getLastError());
+        $this->assertSame([
+            $attribute1,
+            $attribute2,
+            $attribute,
+        ], $dataSource->getAttributes());
+        $this->assertSame($refreshToken, $dataSource->getRefreshToken());
+        $this->assertSame($refreshTokenUrl, $dataSource->getRefreshTokenUrl());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<dataSource id="$id" name="$name" l="$folderId" isEnabled="true" importOnly="true" host="$host" port="$port" connectionType="cleartext" username="$username" password="$password" pollingInterval="$pollingInterval" emailAddress="$emailAddress" smtpEnabled="true" smtpHost="$smtpHost" smtpPort="$smtpPort" smtpConnectionType="cleartext" smtpAuthRequired="true" smtpUsername="$smtpUsername" smtpPassword="$smtpPassword" useAddressForForwardReply="true" defaultSignature="$defaultSignature" forwardReplySignature="$forwardReplySignature" fromDisplay="$fromDisplay" replyToAddress="$replyToAddress" replyToDisplay="$replyToDisplay" importClass="$importClass" failingSince="$failingSince" refreshToken="$refreshToken" refreshTokenUrl="$refreshTokenUrl">
+    <lastError>$lastError</lastError>
+    <a>$attribute1</a>
+    <a>$attribute2</a>
+    <a>$attribute</a>
+</dataSource>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($dataSource, 'xml'));
+        $this->assertEquals($dataSource, $this->serializer->deserialize($xml, MailDataSource::class, 'xml'));
+
+        $json = json_encode([
+            'id' => $id,
+            'name' => $name,
+            'l' => $folderId,
+            'isEnabled' => TRUE,
+            'importOnly' => TRUE,
+            'host' => $host,
+            'port' => $port,
+            'connectionType' => 'cleartext',
+            'username' => $username,
+            'password' => $password,
+            'pollingInterval' => $pollingInterval,
+            'emailAddress' => $emailAddress,
+            'smtpEnabled' => TRUE,
+            'smtpHost' => $smtpHost,
+            'smtpPort' => $smtpPort,
+            'smtpConnectionType' => 'cleartext',
+            'smtpAuthRequired' => TRUE,
+            'smtpUsername' => $smtpUsername,
+            'smtpPassword' => $smtpPassword,
+            'useAddressForForwardReply' => TRUE,
+            'defaultSignature' => $defaultSignature,
+            'forwardReplySignature' => $forwardReplySignature,
+            'fromDisplay' => $fromDisplay,
+            'replyToAddress' => $replyToAddress,
+            'replyToDisplay' => $replyToDisplay,
+            'importClass' => $importClass,
+            'failingSince' => $failingSince,
+            'lastError' => [
+                '_content' => $lastError,
+            ],
+            'a' => [
+                [
+                    '_content' => $attribute1,
+                ],
+                [
+                    '_content' => $attribute2,
+                ],
+                [
+                    '_content' => $attribute,
+                ],
+            ],
+            'refreshToken' => $refreshToken,
+            'refreshTokenUrl' => $refreshTokenUrl,
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($dataSource, 'json'));
+        $this->assertEquals($dataSource, $this->serializer->deserialize($json, MailDataSource::class, 'json'));
+    }
+}
