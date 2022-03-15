c0c1f9c6906287de9f934e3528e15f21b69fec2b
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-03-15 11:05:36 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ tests/Mail/Struct/ExcludeRecurrenceInfoTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Struct;
+
+use Zimbra\Mail\Struct\ExcludeRecurrenceInfo;
+use Zimbra\Mail\Struct\RecurrenceInfo;
+use Zimbra\Struct\ExcludeRecurrenceInfoInterface;
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for ExcludeRecurrenceInfo.
+ */
+class ExcludeRecurrenceInfoTest extends ZimbraTestCase
+{
+    public function testExcludeRecurrenceInfo()
+    {
+        $exclude = new ExcludeRecurrenceInfo();
+        $this->assertTrue($exclude instanceof RecurrenceInfo);
+        $this->assertTrue($exclude instanceof ExcludeRecurrenceInfoInterface);
+    }
+}
