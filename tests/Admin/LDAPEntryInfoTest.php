fe2c44ec9ec5bb58d072657c369785a93901c412
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-05-30 09:02:51 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ tests/Admin/LDAPEntryInfoTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Admin\Struct;
+
+use Zimbra\Admin\Struct\LDAPEntryInfo;
+use Zimbra\Admin\Struct\Attr;
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for LDAPEntryInfo.
+ */
+class LDAPEntryInfoTest extends ZimbraTestCase
+{
+    public function testLDAPEntryInfo()
+    {
+        $name = $this->faker->word;
+        $key = $this->faker->word;
+        $value = $this->faker->word;
+
+        $attr = new Attr($key, $value);
+        $ldap = new LDAPEntryInfo($name, [$attr]);
+        $this->assertSame($name, $ldap->getName());
+
+        $ldap = new LDAPEntryInfo('', [$attr]);
+        $ldap->setName($name);
+        $this->assertSame($name, $ldap->getName());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<result name="$name">
+    <a n="$key">$value</a>
+</result>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($ldap, 'xml'));
+        $this->assertEquals($ldap, $this->serializer->deserialize($xml, LDAPEntryInfo::class, 'xml'));
+
+        $json = json_encode([
+            'name' => $name,
+            'a' => [
+                [
+                    'n' => $key,
+                    '_content' => $value,
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($ldap, 'json'));
+        $this->assertEquals($ldap, $this->serializer->deserialize($json, LDAPEntryInfo::class, 'json'));
+    }
+}
