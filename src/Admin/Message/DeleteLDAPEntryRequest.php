00967f49d0735e75ad26ac69a96ec56474ee171a
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-05-30 09:24:38 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Admin/Message/DeleteLDAPEntryRequest.php
@@ -0,0 +1,83 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Admin\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlAttribute};
+use Zimbra\Soap\Request;
+
+/**
+ * DeleteLDAPEntryRequest class
+ * Delete an LDAP entry
+ * 
+ * @package    Zimbra
+ * @subpackage Admin
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class DeleteLDAPEntryRequest extends Request
+{
+    /**
+     * A valid LDAP DN String (RFC 2253) that describes the DN to delete
+     * @Accessor(getter="getDn", setter="setDn")
+     * @SerializedName("dn")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $dn;
+
+    /**
+     * Constructor method for DeleteLDAPEntryRequest
+     * 
+     * @param  string $dn
+     * @return self
+     */
+    public function __construct(string $dn)
+    {
+        $this->setDn($dn);
+    }
+
+    /**
+     * Gets dn
+     *
+     * @return string
+     */
+    public function getDn(): string
+    {
+        return $this->dn;
+    }
+
+    /**
+     * Sets dn
+     *
+     * @param  string $dn
+     * @return self
+     */
+    public function setDn(string $dn): self
+    {
+        $this->dn = $dn;
+        return $this;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return void
+     */
+    protected function envelopeInit(): void
+    {
+        if (!($this->envelope instanceof DeleteLDAPEntryEnvelope)) {
+            $this->envelope = new DeleteLDAPEntryEnvelope(
+                new DeleteLDAPEntryBody($this)
+            );
+        }
+    }
+}
---
+++ tests/Admin/Message/DeleteLDAPEntryTest.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Admin\Message;
+
+use Zimbra\Admin\Message\DeleteLDAPEntryBody;
+use Zimbra\Admin\Message\DeleteLDAPEntryEnvelope;
+use Zimbra\Admin\Message\DeleteLDAPEntryRequest;
+use Zimbra\Admin\Message\DeleteLDAPEntryResponse;
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for DeleteLDAPEntry.
+ */
+class DeleteLDAPEntryTest extends ZimbraTestCase
+{
+    public function testDeleteLDAPEntry()
+    {
+        $dn = $this->faker->name;
+        $request = new DeleteLDAPEntryRequest($dn);
+        $this->assertSame($dn, $request->getDn());
+        $request = new DeleteLDAPEntryRequest('');
+        $request->setDn($dn);
+        $this->assertSame($dn, $request->getDn());
+
+        $response = new DeleteLDAPEntryResponse();
+
+        $body = new DeleteLDAPEntryBody($request, $response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+        $body = new DeleteLDAPEntryBody();
+        $body->setRequest($request)
+             ->setResponse($response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+
+        $envelope = new DeleteLDAPEntryEnvelope($body);
+        $this->assertSame($body, $envelope->getBody());
+        $envelope = new DeleteLDAPEntryEnvelope();
+        $envelope->setBody($body);
+        $this->assertSame($body, $envelope->getBody());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:zimbraAdmin">
+    <soap:Body>
+        <urn:DeleteLDAPEntryRequest dn="$dn" />
+        <urn:DeleteLDAPEntryResponse />
+    </soap:Body>
+</soap:Envelope>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($envelope, 'xml'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($xml, DeleteLDAPEntryEnvelope::class, 'xml'));
+
+        $json = json_encode([
+            'Body' => [
+                'DeleteLDAPEntryRequest' => [
+                    'dn' => $dn,
+                    '_jsns' => 'urn:zimbraAdmin',
+                ],
+                'DeleteLDAPEntryResponse' => [
+                    '_jsns' => 'urn:zimbraAdmin',
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($envelope, 'json'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($json, DeleteLDAPEntryEnvelope::class, 'json'));
+    }
+}
