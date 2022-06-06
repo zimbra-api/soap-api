48ccda2aa82b58260a34f77a4c9b0bfde0090e12
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-06-06 15:28:44 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ tests/Mail/Message/CreateSearchFolderTest.php
@@ -0,0 +1,119 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Message;
+
+use Zimbra\Common\Enum\ItemType;
+use Zimbra\Common\Enum\SearchSortBy;
+
+use Zimbra\Mail\Message\CreateSearchFolderEnvelope;
+use Zimbra\Mail\Message\CreateSearchFolderBody;
+use Zimbra\Mail\Message\CreateSearchFolderRequest;
+use Zimbra\Mail\Message\CreateSearchFolderResponse;
+
+use Zimbra\Mail\Struct\NewSearchFolderSpec;
+use Zimbra\Mail\Struct\SearchFolder;
+
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for CreateSearchFolder.
+ */
+class CreateSearchFolderTest extends ZimbraTestCase
+{
+    public function testCreateSearchFolder()
+    {
+        $id = $this->faker->uuid;
+        $uuid = $this->faker->uuid;
+        $name = $this->faker->word;
+        $query = $this->faker->word;
+        $searchTypes = implode(',', [ItemType::MESSAGE(), ItemType::CONVERSATION()]);
+        $sortBy = SearchSortBy::DATE_DESC();
+        $flags = $this->faker->word;
+        $color = $this->faker->numberBetween(0, 127);
+        $rgb = $this->faker->hexcolor;
+        $parentFolderId = $this->faker->uuid;
+
+        $newSearch = new NewSearchFolderSpec(
+            $name, $query, $searchTypes, $sortBy, $flags, $color, $rgb, $parentFolderId
+        );
+        $request = new CreateSearchFolderRequest($newSearch);
+        $this->assertSame($newSearch, $request->getSearchFolder());
+        $request = new CreateSearchFolderRequest(new NewSearchFolderSpec('', ''));
+        $request->setSearchFolder($newSearch);
+        $this->assertSame($newSearch, $request->getSearchFolder());
+
+        $search = new SearchFolder(
+            $id,
+            $uuid,
+            $query,
+            $sortBy,
+            $searchTypes
+        );
+        $response = new CreateSearchFolderResponse($search);
+        $this->assertSame($search, $response->getSearchFolder());
+        $response = new CreateSearchFolderResponse(new SearchFolder('', ''));
+        $response->setSearchFolder($search);
+        $this->assertSame($search, $response->getSearchFolder());
+
+        $body = new CreateSearchFolderBody($request, $response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+        $body = new CreateSearchFolderBody();
+        $body->setRequest($request)
+            ->setResponse($response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+
+        $envelope = new CreateSearchFolderEnvelope($body);
+        $this->assertSame($body, $envelope->getBody());
+        $envelope = new CreateSearchFolderEnvelope();
+        $envelope->setBody($body);
+        $this->assertSame($body, $envelope->getBody());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:zimbraMail">
+    <soap:Body>
+        <urn:CreateSearchFolderRequest>
+            <search name="$name" query="$query" types="$searchTypes" sortBy="dateDesc" f="$flags" color="$color" rgb="$rgb" l="$parentFolderId" />
+        </urn:CreateSearchFolderRequest>
+        <urn:CreateSearchFolderResponse>
+            <search id="$id" uuid="$uuid" query="$query" sortBy="dateDesc" types="$searchTypes" />
+        </urn:CreateSearchFolderResponse>
+    </soap:Body>
+</soap:Envelope>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($envelope, 'xml'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($xml, CreateSearchFolderEnvelope::class, 'xml'));
+
+        $json = json_encode([
+            'Body' => [
+                'CreateSearchFolderRequest' => [
+                    'search' => [
+                        'name' => $name,
+                        'query' => $query,
+                        'types' => $searchTypes,
+                        'sortBy' => 'dateDesc',
+                        'f' => $flags,
+                        'rgb' => $rgb,
+                        'color' => $color,
+                        'l' => $parentFolderId,
+                    ],
+                    '_jsns' => 'urn:zimbraMail',
+                ],
+                'CreateSearchFolderResponse' => [
+                    'search' => [
+                        'id' => $id,
+                        'uuid' => $uuid,
+                        'query' => $query,
+                        'sortBy' => 'dateDesc',
+                        'types' => $searchTypes,
+                    ],
+                    '_jsns' => 'urn:zimbraMail',
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($envelope, 'json'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($json, CreateSearchFolderEnvelope::class, 'json'));
+    }
+}
---
+++ tests/Mail/Struct/NewSearchFolderSpecTest.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Struct;
+
+use Zimbra\Common\Enum\ItemType;
+use Zimbra\Common\Enum\SearchSortBy;
+use Zimbra\Mail\Struct\NewSearchFolderSpec;
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for NewSearchFolderSpec.
+ */
+class NewSearchFolderSpecTest extends ZimbraTestCase
+{
+    public function testNewSearchFolderSpec()
+    {
+        $name = $this->faker->word;
+        $query = $this->faker->word;
+        $searchTypes = implode(',', [ItemType::MESSAGE(), ItemType::CONVERSATION()]);
+        $sortBy = SearchSortBy::DATE_DESC();
+        $flags = $this->faker->word;
+        $color = $this->faker->numberBetween(0, 127);
+        $rgb = $this->faker->hexcolor;
+        $parentFolderId = $this->faker->uuid;
+
+        $folder = new NewSearchFolderSpec(
+            $name, $query, $searchTypes, $sortBy, $flags, $color, $rgb, $parentFolderId
+        );
+        $this->assertSame($name, $folder->getName());
+        $this->assertSame($query, $folder->getQuery());
+        $this->assertSame($searchTypes, $folder->getSearchTypes());
+        $this->assertSame($sortBy, $folder->getSortBy());
+        $this->assertSame($flags, $folder->getFlags());
+        $this->assertSame($color, $folder->getColor());
+        $this->assertSame($rgb, $folder->getRgb());
+        $this->assertSame($parentFolderId, $folder->getParentFolderId());
+
+        $folder = new NewSearchFolderSpec('', '');
+        $folder->setName($name)
+            ->setQuery($query)
+            ->setSearchTypes($searchTypes)
+            ->setSortBy($sortBy)
+            ->setFlags($flags)
+            ->setColor($color)
+            ->setRgb($rgb)
+            ->setParentFolderId($parentFolderId);
+        $this->assertSame($name, $folder->getName());
+        $this->assertSame($query, $folder->getQuery());
+        $this->assertSame($searchTypes, $folder->getSearchTypes());
+        $this->assertSame($sortBy, $folder->getSortBy());
+        $this->assertSame($flags, $folder->getFlags());
+        $this->assertSame($color, $folder->getColor());
+        $this->assertSame($rgb, $folder->getRgb());
+        $this->assertSame($parentFolderId, $folder->getParentFolderId());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<result name="$name" query="$query" types="$searchTypes" sortBy="dateDesc" f="$flags" color="$color" rgb="$rgb" l="$parentFolderId" />
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($folder, 'xml'));
+        $this->assertEquals($folder, $this->serializer->deserialize($xml, NewSearchFolderSpec::class, 'xml'));
+
+        $json = json_encode([
+            'name' => $name,
+            'query' => $query,
+            'types' => $searchTypes,
+            'sortBy' => 'dateDesc',
+            'f' => $flags,
+            'rgb' => $rgb,
+            'color' => $color,
+            'l' => $parentFolderId,
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($folder, 'json'));
+        $this->assertEquals($folder, $this->serializer->deserialize($json, NewSearchFolderSpec::class, 'json'));
+    }
+}
