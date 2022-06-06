afb96ce7d86bb85dc966cdf440d6838f34844f93
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-06-06 16:48:08 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/CreateTaskExceptionRequest.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlElement};
+use Zimbra\Soap\EnvelopeInterface;
+
+/**
+ * CreateTaskExceptionRequest class
+ * Create Task Exception.
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class CreateTaskExceptionRequest extends CreateAppointmentExceptionRequest
+{
+    /**
+     * Initialize the soap envelope
+     *
+     * @return EnvelopeInterface
+     */
+    protected function envelopeInit(): EnvelopeInterface
+    {
+        return new CreateTaskExceptionEnvelope(
+            new CreateTaskExceptionBody($this)
+        );
+    }
+}
