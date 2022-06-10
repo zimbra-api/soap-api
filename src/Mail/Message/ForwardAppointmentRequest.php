e6006b21f2c860315f82a4eca15e7f7ce115c1ec
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-06-10 10:56:28 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/ForwardAppointmentRequest.php
@@ -0,0 +1,198 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlAttribute, XmlElement};
+use Zimbra\Mail\Struct\{CalTZInfo, DtTimeInfo, Msg};
+use Zimbra\Soap\{EnvelopeInterface, Request};
+
+/**
+ * ForwardAppointmentRequest class
+ * Used by an attendee to forward an instance or entire appointment to another user who is not already an attendee.
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class ForwardAppointmentRequest extends Request
+{
+    /**
+     * Appointment item ID
+     * 
+     * @Accessor(getter="getId", setter="setId")
+     * @SerializedName("id")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $id;
+
+    /**
+     * RECURRENCE-ID information if forwarding a single instance of a recurring appointment
+     * 
+     * @Accessor(getter="getExceptionId", setter="setExceptionId")
+     * @SerializedName("exceptId")
+     * @Type("Zimbra\Mail\Struct\DtTimeInfo")
+     * @XmlElement
+     */
+    private ?DtTimeInfo $exceptionId = NULL;
+
+    /**
+     * Definition for TZID referenced by DATETIME in <exceptId>
+     * 
+     * @Accessor(getter="getTimezone", setter="setTimezone")
+     * @SerializedName("tz")
+     * @Type("Zimbra\Mail\Struct\CalTZInfo")
+     * @XmlElement
+     */
+    private ?CalTZInfo $timezone = NULL;
+
+    /**
+     * Details of the appointment
+     * 
+     * @Accessor(getter="getMsg", setter="setMsg")
+     * @SerializedName("m")
+     * @Type("Zimbra\Mail\Struct\Msg")
+     * @XmlElement
+     */
+    private ?Msg $msg = NULL;
+
+    /**
+     * Constructor method for ForwardAppointmentRequest
+     *
+     * @param  string $id
+     * @param  DtTimeInfo $exceptionId
+     * @param  CalTZInfo $timezone
+     * @param  Msg $msg
+     * @return self
+     */
+    public function __construct(
+        ?string $id = NULL,
+        ?DtTimeInfo $exceptionId = NULL,
+        ?CalTZInfo $timezone = NULL,
+        ?Msg $msg = NULL
+    )
+    {
+        if (NULL !== $id) {
+            $this->setId($id);
+        }
+        if ($exceptionId instanceof DtTimeInfo) {
+            $this->setExceptionId($exceptionId);
+        }
+        if ($timezone instanceof CalTZInfo) {
+            $this->setTimezone($timezone);
+        }
+        if ($msg instanceof Msg) {
+            $this->setMsg($msg);
+        }
+    }
+
+    /**
+     * Sets msg
+     *
+     * @param  Msg $msg
+     * @return self
+     */
+    public function setMsg(Msg $msg): self
+    {
+        $this->msg = $msg;
+        return $this;
+    }
+
+    /**
+     * Gets msg
+     *
+     * @return Msg
+     */
+    public function getMsg(): ?Msg
+    {
+        return $this->msg;
+    }
+
+    /**
+     * Sets exceptionId
+     *
+     * @param  DtTimeInfo $exceptionId
+     * @return self
+     */
+    public function setExceptionId(DtTimeInfo $exceptionId): self
+    {
+        $this->exceptionId = $exceptionId;
+        return $this;
+    }
+
+    /**
+     * Gets exceptionId
+     *
+     * @return DtTimeInfo
+     */
+    public function getExceptionId(): ?DtTimeInfo
+    {
+        return $this->exceptionId;
+    }
+
+    /**
+     * Sets timezone
+     *
+     * @param  CalTZInfo $timezone
+     * @return self
+     */
+    public function setTimezone(CalTZInfo $timezone): self
+    {
+        $this->timezone = $timezone;
+        return $this;
+    }
+
+    /**
+     * Gets timezone
+     *
+     * @return CalTZInfo
+     */
+    public function getTimezone(): ?CalTZInfo
+    {
+        return $this->timezone;
+    }
+
+    /**
+     * Sets id
+     *
+     * @param  string $id
+     * @return self
+     */
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Gets id
+     *
+     * @return string
+     */
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return EnvelopeInterface
+     */
+    protected function envelopeInit(): EnvelopeInterface
+    {
+        return new ForwardAppointmentEnvelope(
+            new ForwardAppointmentBody($this)
+        );
+    }
+}
