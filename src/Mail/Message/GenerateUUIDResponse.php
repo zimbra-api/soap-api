11fb246e4f308195a15e46e87641ab91e4a05574
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-06-10 11:04:17 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/GenerateUUIDResponse.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlValue};
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * GenerateUUIDResponse class
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class GenerateUUIDResponse implements ResponseInterface
+{
+    /**
+     * Generated globally unique UUID
+     * 
+     * @Accessor(getter="getUuid", setter="setUuid")
+     * @SerializedName("_content")
+     * @Type("string")
+     * @XmlValue
+     */
+    private $uuid;
+
+    /**
+     * Constructor method for GenerateUUIDResponse
+     *
+     * @param  string $uuid
+     * @return self
+     */
+    public function __construct(string $uuid)
+    {
+        $this->setUuid($uuid);
+    }
+
+    /**
+     * Gets uuid
+     *
+     * @return string
+     */
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * Sets uuid
+     *
+     * @param  string $uuid
+     * @return self
+     */
+    public function setUuid(string $uuid): self
+    {
+        $this->uuid = $uuid;
+        return $this;
+    }
+}
