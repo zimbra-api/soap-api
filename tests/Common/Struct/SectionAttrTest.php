32867beedb6ce0d4b878bc2821a8df3de02e6d08
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-06-18 08:48:40 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ tests/Common/Struct/SectionAttrTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Common\Struct;
+
+use Zimbra\Common\Struct\SectionAttr;
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for SectionAttr.
+ */
+class SectionAttrTest extends ZimbraTestCase
+{
+    public function testSectionAttr()
+    {
+        $section = $this->faker->word;
+
+        $attr = new SectionAttr($section);
+        $this->assertSame($section, $attr->getSection());
+
+        $attr = new SectionAttr('');
+        $attr->setSection($section);
+        $this->assertSame($section, $attr->getSection());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<result section="$section" />
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($attr, 'xml'));
+        $this->assertEquals($attr, $this->serializer->deserialize($xml, SectionAttr::class, 'xml'));
+
+        $json = json_encode([
+            'section' => $section,
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($attr, 'json'));
+        $this->assertEquals($attr, $this->serializer->deserialize($json, SectionAttr::class, 'json'));
+    }
+}
