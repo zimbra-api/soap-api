c8c2049ea3df5f3d589d4b02622aaaf08df5b1dd
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-06-19 08:18:26 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ tests/Mail/Struct/GetFolderSpecTest.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Struct;
+
+use Zimbra\Mail\Struct\GetFolderSpec;
+
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for GetFolderSpec.
+ */
+class GetFolderSpecTest extends ZimbraTestCase
+{
+    public function testGetFolderSpec()
+    {
+        $uuid = $this->faker->uuid;
+        $folderId = $this->faker->uuid;
+        $path = $this->faker->word;
+
+        $folder = new GetFolderSpec($uuid, $folderId, $path);
+        $this->assertSame($uuid, $folder->getUuid());
+        $this->assertSame($folderId, $folder->getFolderId());
+        $this->assertSame($path, $folder->getPath());
+
+        $folder = new GetFolderSpec();
+        $folder->setUuid($uuid)
+             ->setFolderId($folderId)
+             ->setPath($path);
+        $this->assertSame($uuid, $folder->getUuid());
+        $this->assertSame($folderId, $folder->getFolderId());
+        $this->assertSame($path, $folder->getPath());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<result uuid="$uuid" l="$folderId" path="$path" />
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($folder, 'xml'));
+        $this->assertEquals($folder, $this->serializer->deserialize($xml, GetFolderSpec::class, 'xml'));
+
+        $json = json_encode([
+            'uuid' => $uuid,
+            'l' => $folderId,
+            'path' => $path,
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($folder, 'json'));
+        $this->assertEquals($folder, $this->serializer->deserialize($json, GetFolderSpec::class, 'json'));
+    }
+}
