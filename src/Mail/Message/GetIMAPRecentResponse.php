64d5cf7c3e417483067a6995aa48e9738f19025c
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-06-20 15:19:47 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/GetIMAPRecentResponse.php
@@ -0,0 +1,68 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlAttribute};
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * GetIMAPRecentResponse class
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class GetIMAPRecentResponse implements ResponseInterface
+{
+    /**
+     * Number of recent items
+     * @Accessor(getter="getNum", setter="setNum")
+     * @SerializedName("n")
+     * @Type("integer")
+     * @XmlAttribute
+     */
+    private $num;
+
+    /**
+     * Constructor method for GetIMAPRecentResponse
+     *
+     * @param  int $num
+     * @return self
+     */
+    public function __construct(int $num)
+    {
+        $this->setNum($num);
+    }
+
+    /**
+     * Gets num
+     *
+     * @return int
+     */
+    public function getNum(): int
+    {
+        return $this->num;
+    }
+
+    /**
+     * Sets num
+     *
+     * @param  int $num
+     * @return self
+     */
+    public function setNum(int $num): self
+    {
+        $this->num = $num;
+        return $this;
+    }
+}
---
+++ tests/Mail/Message/GetIMAPRecentTest.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Message;
+
+use Zimbra\Mail\Message\GetIMAPRecentEnvelope;
+use Zimbra\Mail\Message\GetIMAPRecentBody;
+use Zimbra\Mail\Message\GetIMAPRecentRequest;
+use Zimbra\Mail\Message\GetIMAPRecentResponse;
+
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for GetIMAPRecent.
+ */
+class GetIMAPRecentTest extends ZimbraTestCase
+{
+    public function testGetIMAPRecent()
+    {
+        $id = $this->faker->uuid;
+        $num = $this->faker->randomNumber;
+
+        $request = new GetIMAPRecentRequest($id);
+        $this->assertSame($id, $request->getId());
+        $request = new GetIMAPRecentRequest('');
+        $request->setId($id);
+        $this->assertSame($id, $request->getId());
+
+        $response = new GetIMAPRecentResponse($num);
+        $this->assertSame($num, $response->getNum());
+        $response = new GetIMAPRecentResponse(0);
+        $response->setNum($num);
+        $this->assertSame($num, $response->getNum());
+
+        $body = new GetIMAPRecentBody($request, $response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+        $body = new GetIMAPRecentBody();
+        $body->setRequest($request)
+            ->setResponse($response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+
+        $envelope = new GetIMAPRecentEnvelope($body);
+        $this->assertSame($body, $envelope->getBody());
+        $envelope = new GetIMAPRecentEnvelope();
+        $envelope->setBody($body);
+        $this->assertSame($body, $envelope->getBody());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:zimbraMail">
+    <soap:Body>
+        <urn:GetIMAPRecentRequest id="$id" />
+        <urn:GetIMAPRecentResponse n="$num" />
+    </soap:Body>
+</soap:Envelope>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($envelope, 'xml'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($xml, GetIMAPRecentEnvelope::class, 'xml'));
+
+        $json = json_encode([
+            'Body' => [
+                'GetIMAPRecentRequest' => [
+                    'id' => $id,
+                    '_jsns' => 'urn:zimbraMail',
+                ],
+                'GetIMAPRecentResponse' => [
+                    'n' => $num,
+                    '_jsns' => 'urn:zimbraMail',
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($envelope, 'json'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($json, GetIMAPRecentEnvelope::class, 'json'));
+    }
+}
