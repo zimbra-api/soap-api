c2118bed42c3602d25d01fd15414c97947b38577
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-06-21 13:59:49 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/GetMailboxMetadataResponse.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlElement};
+use Zimbra\Mail\Struct\MailCustomMetadata;
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * GetMailboxMetadataResponse class
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class GetMailboxMetadataResponse implements ResponseInterface
+{
+    /**
+     * Metadata information
+     * @Accessor(getter="getMetadata", setter="setMetadata")
+     * @SerializedName("meta")
+     * @Type("Zimbra\Mail\Struct\MailCustomMetadata")
+     * @XmlElement
+     */
+    private ?MailCustomMetadata $metadata = NULL;
+
+    /**
+     * Constructor method for GetMailboxMetadataResponse
+     *
+     * @param  MailCustomMetadata $metadata
+     * @return self
+     */
+    public function __construct(?MailCustomMetadata $metadata = NULL)
+    {
+        if ($metadata instanceof MailCustomMetadata) {
+            $this->setMetadata($metadata);
+        }
+    }
+
+    /**
+     * Gets metadata
+     *
+     * @return MailCustomMetadata
+     */
+    public function getMetadata(): MailCustomMetadata
+    {
+        return $this->metadata;
+    }
+
+    /**
+     * Sets metadata
+     *
+     * @param  MailCustomMetadata $metadata
+     * @return self
+     */
+    public function setMetadata(MailCustomMetadata $metadata): self
+    {
+        $this->metadata = $metadata;
+        return $this;
+    }
+}
---
+++ tests/Mail/Message/GetMailboxMetadataTest.php
@@ -0,0 +1,101 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Message;
+
+use Zimbra\Common\Struct\KeyValuePair;
+use Zimbra\Common\Struct\SectionAttr;
+
+use Zimbra\Mail\Message\GetMailboxMetadataEnvelope;
+use Zimbra\Mail\Message\GetMailboxMetadataBody;
+use Zimbra\Mail\Message\GetMailboxMetadataRequest;
+use Zimbra\Mail\Message\GetMailboxMetadataResponse;
+
+use Zimbra\Mail\Struct\MailCustomMetadata;
+
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for GetMailboxMetadata.
+ */
+class GetMailboxMetadataTest extends ZimbraTestCase
+{
+    public function testGetMailboxMetadata()
+    {
+        $section = $this->faker->word;
+        $section = $this->faker->word;
+        $key = $this->faker->word;
+        $value = $this->faker->text;
+
+        $meta = new SectionAttr($section);
+        $request = new GetMailboxMetadataRequest($meta);
+        $this->assertSame($meta, $request->getMetadata());
+        $request = new GetMailboxMetadataRequest();
+        $request->setMetadata($meta);
+        $this->assertSame($meta, $request->getMetadata());
+
+        $meta = new MailCustomMetadata($section, [new KeyValuePair($key, $value)]);
+        $response = new GetMailboxMetadataResponse($meta);
+        $this->assertSame($meta, $response->getMetadata());
+        $response = new GetMailboxMetadataResponse();
+        $response->setMetadata($meta);
+        $this->assertSame($meta, $response->getMetadata());
+
+        $body = new GetMailboxMetadataBody($request, $response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+        $body = new GetMailboxMetadataBody();
+        $body->setRequest($request)
+            ->setResponse($response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+
+        $envelope = new GetMailboxMetadataEnvelope($body);
+        $this->assertSame($body, $envelope->getBody());
+        $envelope = new GetMailboxMetadataEnvelope();
+        $envelope->setBody($body);
+        $this->assertSame($body, $envelope->getBody());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:zimbraMail">
+    <soap:Body>
+        <urn:GetMailboxMetadataRequest>
+            <meta section="$section" />
+        </urn:GetMailboxMetadataRequest>
+        <urn:GetMailboxMetadataResponse>
+            <meta section="$section">
+                <a n="$key">$value</a>
+            </meta>
+        </urn:GetMailboxMetadataResponse>
+    </soap:Body>
+</soap:Envelope>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($envelope, 'xml'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($xml, GetMailboxMetadataEnvelope::class, 'xml'));
+
+        $json = json_encode([
+            'Body' => [
+                'GetMailboxMetadataRequest' => [
+                    'meta' => [
+                        'section' => $section,
+                    ],
+                    '_jsns' => 'urn:zimbraMail',
+                ],
+                'GetMailboxMetadataResponse' => [
+                    'meta' => [
+                        'section' => $section,
+                        'a' => [
+                            [
+                                'n' => $key,
+                                '_content' => $value,
+                            ],
+                        ],
+                    ],
+                    '_jsns' => 'urn:zimbraMail',
+                ],
+            ],
+        ]);
+        $this->assertJsonStringEqualsJsonString($json, $this->serializer->serialize($envelope, 'json'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($json, GetMailboxMetadataEnvelope::class, 'json'));
+    }
+}
