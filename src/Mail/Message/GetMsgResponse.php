ea6483b6a85d667d4f1e2e6eb6ae0dc0387870a4
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-07-05 15:30:52 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/GetMsgResponse.php
@@ -0,0 +1,72 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlElement};
+use Zimbra\Mail\Struct\MsgWithGroupInfo;
+use Zimbra\Soap\ResponseInterface;
+
+/**
+ * GetMsgResponse class
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class GetMsgResponse implements ResponseInterface
+{
+    /**
+     * Message information
+     * 
+     * @Accessor(getter="getMsg", setter="setMsg")
+     * @SerializedName("m")
+     * @Type("Zimbra\Mail\Struct\MsgWithGroupInfo")
+     * @XmlElement(namespace="urn:zimbraMail")
+     */
+    private ?MsgWithGroupInfo $msg = NULL;
+
+    /**
+     * Constructor method for GetMsgResponse
+     *
+     * @param  MsgWithGroupInfo $msg
+     * @return self
+     */
+    public function __construct(?MsgWithGroupInfo $msg = NULL)
+    {
+        if ($msg instanceof MsgWithGroupInfo) {
+            $this->setMsg($msg);
+        }
+    }
+
+    /**
+     * Gets msg
+     *
+     * @return MsgWithGroupInfo
+     */
+    public function getMsg(): ?MsgWithGroupInfo
+    {
+        return $this->msg;
+    }
+
+    /**
+     * Sets msg
+     *
+     * @param  MsgWithGroupInfo $msg
+     * @return self
+     */
+    public function setMsg(MsgWithGroupInfo $msg): self
+    {
+        $this->msg = $msg;
+        return $this;
+    }
+}
