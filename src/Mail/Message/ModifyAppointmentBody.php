922c565e2a760fa4e100f63868d8df06c227af08
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-07-08 10:05:56 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/ModifyAppointmentBody.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlElement};
+use Zimbra\Soap\{Body, RequestInterface, ResponseInterface};
+
+/**
+ * ModifyAppointmentBody class
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class ModifyAppointmentBody extends Body
+{
+    /**
+     * @Accessor(getter="getRequest", setter="setRequest")
+     * @SerializedName("ModifyAppointmentRequest")
+     * @Type("Zimbra\Mail\Message\ModifyAppointmentRequest")
+     * @XmlElement(namespace="urn:zimbraMail")
+     */
+    private ?RequestInterface $request = NULL;
+
+    /**
+     * @Accessor(getter="getResponse", setter="setResponse")
+     * @SerializedName("ModifyAppointmentResponse")
+     * @Type("Zimbra\Mail\Message\ModifyAppointmentResponse")
+     * @XmlElement(namespace="urn:zimbraMail")
+     */
+    private ?ResponseInterface $response = NULL;
+
+    /**
+     * Constructor method for ModifyAppointmentBody
+     *
+     * @return self
+     */
+    public function __construct(
+        ?ModifyAppointmentRequest $request = NULL, ?ModifyAppointmentResponse $response = NULL
+    )
+    {
+        parent::__construct($request, $response);
+    }
+
+    public function setRequest(RequestInterface $request): self
+    {
+        if ($request instanceof ModifyAppointmentRequest) {
+            $this->request = $request;
+        }
+        return $this;
+    }
+
+    public function getRequest(): ?RequestInterface
+    {
+        return $this->request;
+    }
+
+    public function setResponse(ResponseInterface $response): self
+    {
+        if ($response instanceof ModifyAppointmentResponse) {
+            $this->response = $response;
+        }
+        return $this;
+    }
+
+    public function getResponse(): ?ResponseInterface
+    {
+        return $this->response;
+    }
+}
---
+++ tests/Mail/Message/ModifyAppointmentTest.php
@@ -0,0 +1,192 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Message;
+
+use Zimbra\Common\Enum\AddressType;
+use Zimbra\Common\Enum\ReplyType;
+use Zimbra\Common\Enum\InviteType;
+
+use Zimbra\Mail\Message\ModifyAppointmentEnvelope;
+use Zimbra\Mail\Message\ModifyAppointmentBody;
+use Zimbra\Mail\Message\ModifyAppointmentRequest;
+use Zimbra\Mail\Message\ModifyAppointmentResponse;
+
+use Zimbra\Mail\Struct\AttachmentsInfo;
+use Zimbra\Mail\Struct\CalTZInfo;
+use Zimbra\Mail\Struct\EmailAddrInfo;
+use Zimbra\Mail\Struct\Header;
+use Zimbra\Mail\Struct\InstanceRecurIdInfo;
+use Zimbra\Mail\Struct\InvitationInfo;
+use Zimbra\Mail\Struct\MimePartInfo;
+use Zimbra\Mail\Struct\Msg;
+
+use Zimbra\Mail\Struct\EmailInfo;
+use Zimbra\Mail\Struct\MPInviteInfo;
+use Zimbra\Mail\Struct\InviteAsMP;
+use Zimbra\Mail\Struct\CalEcho;
+use Zimbra\Mail\Struct\PartInfo;
+use Zimbra\Mail\Struct\ShareNotification;
+use Zimbra\Mail\Struct\DLSubscriptionNotification;
+
+use Zimbra\Common\Struct\KeyValuePair;
+use Zimbra\Common\Struct\Id;
+
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for ModifyAppointment.
+ */
+class ModifyAppointmentTest extends ZimbraTestCase
+{
+    public function testModifyAppointment()
+    {
+        $id = $this->faker->uuid;
+        $componentNum = $this->faker->randomNumber;
+        $modifiedSequence = $this->faker->randomNumber;
+        $revision = $this->faker->randomNumber;
+        $tzStdOffset = $this->faker->randomNumber;
+        $tzDayOffset = $this->faker->randomNumber;
+
+        $origId = $this->faker->uuid;
+        $replyType = ReplyType::REPLIED();
+        $identityId = $this->faker->uuid;
+        $subject = $this->faker->text;
+        $inReplyTo = $this->faker->uuid;
+        $folderId = $this->faker->uuid;
+        $flags = $this->faker->word;
+        $content = $this->faker->text;
+        $fragment = $this->faker->text;
+        $contentType = $this->faker->word;
+        $contentId = $this->faker->uuid;
+        $name = $this->faker->name;
+        $key = $this->faker->word;
+        $value = $this->faker->word;
+        $method = $this->faker->word;
+        $address = $this->faker->email;
+        $personal = $this->faker->word;
+        $maxSize = $this->faker->randomNumber;
+
+        $calItemId = $this->faker->uuid;
+        $deprecatedApptId = $this->faker->uuid;
+        $calInvId = $this->faker->uuid;
+
+        $part = $this->faker->word;
+        $sentDate = $this->faker->unixTime;
+        $messageIdHeader = $this->faker->uuid;
+
+        $display = $this->faker->name;
+        $addressType = AddressType::TO();
+        $calItemType = InviteType::TASK();
+
+        $size = $this->faker->randomNumber;
+        $contentDisposition = $this->faker->word;
+        $contentFilename = $this->faker->word;
+        $location = $this->faker->word;
+
+        $msg = new Msg(
+            $id, $origId, $replyType, $identityId, $subject,
+            [new Header($name, $value)], $inReplyTo, $folderId, $flags, $content,
+            new MimePartInfo($contentType, $content, $contentId),
+            new AttachmentsInfo($id),
+            new InvitationInfo($method, $componentNum, TRUE),
+            [new EmailAddrInfo($address, AddressType::TO(), $personal)],
+            [new CalTZInfo($id, $tzStdOffset, $tzDayOffset)],
+            $fragment
+        );
+
+        $request = new ModifyAppointmentRequest(
+            $id, $componentNum, $modifiedSequence, $revision
+        );
+        $this->assertSame($id, $request->getId());
+        $this->assertSame($componentNum, $request->getComponentNum());
+        $this->assertSame($modifiedSequence, $request->getModifiedSequence());
+        $this->assertSame($revision, $request->getRevision());
+        $request = new ModifyAppointmentRequest(
+            '', 0, 0, 0, $msg, TRUE, $maxSize, TRUE, TRUE, TRUE
+        );
+        $request->setId($id)
+            ->setComponentNum($componentNum)
+            ->setModifiedSequence($modifiedSequence)
+            ->setRevision($revision);
+        $this->assertSame($id, $request->getId());
+        $this->assertSame($componentNum, $request->getComponentNum());
+        $this->assertSame($modifiedSequence, $request->getModifiedSequence());
+        $this->assertSame($revision, $request->getRevision());
+
+        $contentElems = [
+            new PartInfo(
+                $part, $contentType, $size, $contentDisposition, $contentFilename, $contentId, $location, TRUE, TRUE, $content
+            ),
+            new ShareNotification(TRUE, $content),
+            new DLSubscriptionNotification(TRUE, $content),
+        ];
+        $invite = new InviteAsMP(
+            $id, $part, $sentDate,
+            [new EmailInfo($address, $display, $personal, $addressType)],
+            $subject, $messageIdHeader,
+            new MPInviteInfo($calItemType), [new KeyValuePair($key, $value)],
+            $contentElems
+        );
+        $response = new ModifyAppointmentResponse(
+            $calItemId, $deprecatedApptId, $calInvId, $modifiedSequence, $revision, new Id($id), new CalEcho($invite)
+        );
+
+        $body = new ModifyAppointmentBody($request, $response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+        $body = new ModifyAppointmentBody();
+        $body->setRequest($request)
+            ->setResponse($response);
+        $this->assertSame($request, $body->getRequest());
+        $this->assertSame($response, $body->getResponse());
+
+        $envelope = new ModifyAppointmentEnvelope($body);
+        $this->assertSame($body, $envelope->getBody());
+        $envelope = new ModifyAppointmentEnvelope();
+        $envelope->setBody($body);
+        $this->assertSame($body, $envelope->getBody());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:zimbraMail">
+    <soap:Body>
+        <urn:ModifyAppointmentRequest id="$id" comp="$componentNum" ms="$modifiedSequence" rev="$revision" echo="true" max="$maxSize" want="true" neuter="true" forcesend="true">
+            <urn:m aid="$id" origid="$origId" rt="r" idnt="$identityId" su="$subject" irt="$inReplyTo" l="$folderId" f="$flags">
+                <urn:header name="$name">$value</urn:header>
+                <urn:content>$content</urn:content>
+                <urn:mp ct="$contentType" content="$content" ci="$contentId" />
+                <urn:attach aid="$id" />
+                <urn:inv method="$method" compNum="$componentNum" rsvp="true" />
+                <urn:e a="$address" t="t" p="$personal" />
+                <urn:tz id="$id" stdoff="$tzStdOffset" dayoff="$tzDayOffset" />
+                <urn:fr>$fragment</urn:fr>
+            </urn:m>
+        </urn:ModifyAppointmentRequest>
+        <urn:ModifyAppointmentResponse calItemId="$calItemId" apptId="$deprecatedApptId" invId="$calInvId" ms="$modifiedSequence" rev="$revision">
+            <urn:m id="$id" />
+            <urn:echo>
+                <urn:m id="$id" part="$part" sd="$sentDate">
+                    <urn:e a="$address" d="$display" p="$personal" t="t" />
+                    <urn:su>$subject</urn:su>
+                    <urn:mid>$messageIdHeader</urn:mid>
+                    <urn:inv type="task" />
+                    <urn:header n="$key">$value</urn:header>
+                    <urn:mp part="$part" ct="$contentType" s="$size" cd="$contentDisposition" filename="$contentFilename" ci="$contentId" cl="$location" body="true" truncated="true">
+                        <urn:content>$content</urn:content>
+                    </urn:mp>
+                    <urn:shr truncated="true">
+                        <urn:content>$content</urn:content>
+                    </urn:shr>
+                    <urn:dlSubs truncated="true">
+                        <urn:content>$content</urn:content>
+                    </urn:dlSubs>
+                </urn:m>
+            </urn:echo>
+        </urn:ModifyAppointmentResponse>
+    </soap:Body>
+</soap:Envelope>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($envelope, 'xml'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($xml, ModifyAppointmentEnvelope::class, 'xml'));
+    }
+}
