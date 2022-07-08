67efe214b1fb136ba99c389d49301591fd2f21b5
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-07-08 08:38:44 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/InvalidateReminderDeviceRequest.php
@@ -0,0 +1,82 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlAttribute};
+use Zimbra\Soap\{EnvelopeInterface, Request};
+
+/**
+ * InvalidateReminderDeviceRequest class
+ * Invalidate reminder device
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class InvalidateReminderDeviceRequest extends Request
+{
+    /**
+     * Device email address
+     * 
+     * @Accessor(getter="getAddress", setter="setAddress")
+     * @SerializedName("a")
+     * @Type("string")
+     * @XmlAttribute
+     */
+    private $address;
+
+    /**
+     * Constructor method for InvalidateReminderDeviceRequest
+     *
+     * @param  string $address
+     * @return self
+     */
+    public function __construct(string $address = '')
+    {
+        $this->setAddress($address);
+    }
+
+    /**
+     * Gets address
+     *
+     * @return string
+     */
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    /**
+     * Sets address
+     *
+     * @param  string $address
+     * @return self
+     */
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+        return $this;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return EnvelopeInterface
+     */
+    protected function envelopeInit(): EnvelopeInterface
+    {
+        return new InvalidateReminderDeviceEnvelope(
+            new InvalidateReminderDeviceBody($this)
+        );
+    }
+}
