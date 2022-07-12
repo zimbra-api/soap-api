0a00c56a6c0860c4b2989ff480fb134bfde049b5
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-07-12 10:31:31 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

---
+++ src/Mail/Message/PurgeRevisionRequest.php
@@ -0,0 +1,83 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the Zimbra API in PHP library.
+ *
+ * © Nguyen Van Nguyen <nguyennv1981@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Zimbra\Mail\Message;
+
+use JMS\Serializer\Annotation\{Accessor, SerializedName, Type, XmlElement};
+use Zimbra\Mail\Struct\PurgeRevisionSpec;
+use Zimbra\Soap\{EnvelopeInterface, Request};
+
+/**
+ * PurgeRevisionRequest class
+ * Purge revision
+ * 
+ * @package    Zimbra
+ * @subpackage Mail
+ * @category   Message
+ * @author     Nguyen Van Nguyen - nguyennv1981@gmail.com
+ * @copyright  Copyright © 2013-present by Nguyen Van Nguyen.
+ */
+class PurgeRevisionRequest extends Request
+{
+    /**
+     * Specification or revision to purge
+     * 
+     * @Accessor(getter="getRevision", setter="setRevision")
+     * @SerializedName("revision")
+     * @Type("Zimbra\Mail\Struct\PurgeRevisionSpec")
+     * @XmlElement(namespace="urn:zimbraMail")
+     */
+    private PurgeRevisionSpec $revision;
+
+    /**
+     * Constructor method for PurgeRevisionRequest
+     *
+     * @param  PurgeRevisionSpec $revision
+     * @return self
+     */
+    public function __construct(PurgeRevisionSpec $revision)
+    {
+        $this->setRevision($revision);
+    }
+
+    /**
+     * Gets revision
+     *
+     * @return PurgeRevisionSpec
+     */
+    public function getRevision(): PurgeRevisionSpec
+    {
+        return $this->revision;
+    }
+
+    /**
+     * Sets revision
+     *
+     * @param  PurgeRevisionSpec $revision
+     * @return self
+     */
+    public function setRevision(PurgeRevisionSpec $revision): self
+    {
+        $this->revision = $revision;
+        return $this;
+    }
+
+    /**
+     * Initialize the soap envelope
+     *
+     * @return EnvelopeInterface
+     */
+    protected function envelopeInit(): EnvelopeInterface
+    {
+        return new PurgeRevisionEnvelope(
+            new PurgeRevisionBody($this)
+        );
+    }
+}
