6721742be042655c277ea40b0bbbe8f62e7107c6
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-07-12 15:35:43 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- tests/Mail/Message/SaveDraftTest.php
+++ tests/Mail/Message/SaveDraftTest.php
@@ -3,18 +3,25 @@
 namespace Zimbra\Tests\Mail\Message;
 
 use Zimbra\Common\Enum\AddressType;
-use Zimbra\Common\Enum\InviteType;
 use Zimbra\Common\Enum\ReplyType;
+use Zimbra\Common\Enum\InviteType;
 use Zimbra\Common\Struct\KeyValuePair;
 
-use Zimbra\Mail\Message\RemoveAttachmentsEnvelope;
-use Zimbra\Mail\Message\RemoveAttachmentsBody;
-use Zimbra\Mail\Message\RemoveAttachmentsRequest;
-use Zimbra\Mail\Message\RemoveAttachmentsResponse;
+use Zimbra\Mail\Message\SaveDraftEnvelope;
+use Zimbra\Mail\Message\SaveDraftBody;
+use Zimbra\Mail\Message\SaveDraftRequest;
+use Zimbra\Mail\Message\SaveDraftResponse;
+
+use Zimbra\Mail\Struct\AttachmentsInfo;
+use Zimbra\Mail\Struct\CalTZInfo;
+use Zimbra\Mail\Struct\EmailAddrInfo;
+use Zimbra\Mail\Struct\Header;
+use Zimbra\Mail\Struct\InvitationInfo;
+use Zimbra\Mail\Struct\MimePartInfo;
+use Zimbra\Mail\Struct\SaveDraftMsg;
 
 use Zimbra\Mail\Struct\MsgPartIds;
 use Zimbra\Mail\Struct\CalendarReply;
-use Zimbra\Mail\Struct\CalTZInfo;
 use Zimbra\Mail\Struct\DLSubscriptionNotification;
 use Zimbra\Mail\Struct\EmailInfo;
 use Zimbra\Mail\Struct\InviteComponent;
@@ -27,50 +34,62 @@ use Zimbra\Mail\Struct\ShareNotification;
 use Zimbra\Tests\ZimbraTestCase;
 
 /**
- * Testcase class for RemoveAttachments.
+ * Testcase class for SaveDraft.
  */
-class RemoveAttachmentsTest extends ZimbraTestCase
+class SaveDraftTest extends ZimbraTestCase
 {
-    public function testRemoveAttachments()
+    public function testSaveDraft()
     {
-        $id = $this->faker->uuid;
-        $partIds = $this->faker->word;
+        $id = $this->faker->word;
+        $intId = $this->faker->randomNumber;
+        $origId = $this->faker->uuid;
+        $replyType = ReplyType::REPLIED();
+        $identityId = $this->faker->uuid;
+        $subject = $this->faker->text;
+        $inReplyTo = $this->faker->uuid;
+        $folderId = $this->faker->uuid;
+        $flags = $this->faker->word;
+        $content = $this->faker->text;
+        $fragment = $this->faker->text;
+        $contentType = $this->faker->mimeType;
+        $contentId = $this->faker->uuid;
+        $name = $this->faker->name;
+        $value = $this->faker->word;
+        $method = $this->faker->word;
+        $componentNum = $this->faker->randomNumber;
+        $address = $this->faker->email;
+        $personal = $this->faker->word;
+        $tzStdOffset = $this->faker->randomNumber;
+        $tzDayOffset = $this->faker->randomNumber;
 
+        $draftAccountId = $this->faker->uuid;
+        $tags = $this->faker->word;
+        $tagNames = $this->faker->word;
+        $rgb = $this->faker->hexcolor;
+        $color = $this->faker->numberBetween(0, 127);
+        $autoSendTime = $this->faker->unixTime;
+
+        $partIds = $this->faker->word;
         $imapUid = $this->faker->randomNumber;
         $calendarIntendedFor = $this->faker->word;
-        $origId = $this->faker->uuid;
         $draftReplyType = ReplyType::REPLIED();
-        $identityId = $this->faker->uuid;
-        $draftAccountId = $this->faker->uuid;
         $draftAutoSendTime = $this->faker->randomNumber;
         $sentDate = $this->faker->randomNumber;
         $resentDate = $this->faker->randomNumber;
         $part = $this->faker->word;
         $fragment = $this->faker->word;
-        $subject = $this->faker->word;
         $messageIdHeader = $this->faker->word;
-        $inReplyTo = $this->faker->word;
 
-        $address = $this->faker->email;
         $display = $this->faker->name;
-        $personal = $this->faker->word;
         $addressType = AddressType::TO();
 
-        $contentType = $this->faker->mimeType;
         $size = $this->faker->randomNumber;
         $contentDisposition = $this->faker->word;
         $contentFilename = $this->faker->word;
-        $contentId = $this->faker->word;
         $location = $this->faker->word;
-        $content = $this->faker->text;
 
         $calItemType = InviteType::TASK();
-        $tzStdOffset = $this->faker->randomNumber;
-        $tzDayOffset = $this->faker->randomNumber;
-        $method = $this->faker->word;
-        $componentNum = $this->faker->randomNumber;
         $key = $this->faker->word;
-        $value = $this->faker->word;
 
         $seq = $this->faker->randomNumber;
         $date = $this->faker->unixTime;
@@ -78,15 +97,27 @@ class RemoveAttachmentsTest extends ZimbraTestCase
         $rangeType = $this->faker->numberBetween(1, 3);
         $recurId = $this->faker->uuid;
 
-        $msg = new MsgPartIds(
-            $id, $partIds
+        $header = new Header($name, $value);
+        $mimePart = new MimePartInfo($contentType, $content, $contentId);
+        $attachments = new AttachmentsInfo($id);
+        $invite = new InvitationInfo($method, $componentNum, TRUE);
+        $emailAddress = new EmailAddrInfo($address, AddressType::TO(), $personal);
+        $timezone = new CalTZInfo($id, $tzStdOffset, $tzDayOffset);
+        $msg = new SaveDraftMsg(
+            $id, $origId, $replyType, $identityId, $subject, [$header], $inReplyTo, $folderId, $flags, $content, $mimePart, $attachments, $invite, [$emailAddress], [$timezone], $fragment, $intId, $draftAccountId, $tags, $tagNames, $rgb, $color, $autoSendTime
         );
 
-        $request = new RemoveAttachmentsRequest($msg);
+        $request = new SaveDraftRequest($msg, FALSE, FALSE);
         $this->assertSame($msg, $request->getMsg());
-        $request = new RemoveAttachmentsRequest(new MsgPartIds());
-        $request->setMsg($msg);
+        $this->assertFalse($request->getWantImapUid());
+        $this->assertFalse($request->getWantModifiedSequence());
+        $request = new SaveDraftRequest(new SaveDraftMsg());
+        $request->setMsg($msg)
+            ->setWantImapUid(TRUE)
+            ->setWantModifiedSequence(TRUE);
         $this->assertSame($msg, $request->getMsg());
+        $this->assertTrue($request->getWantImapUid());
+        $this->assertTrue($request->getWantModifiedSequence());
 
         $timezone = new CalTZInfo($id, $tzStdOffset, $tzDayOffset);
         $inviteComponent = new InviteComponent($method, $componentNum, TRUE);
@@ -107,28 +138,28 @@ class RemoveAttachmentsTest extends ZimbraTestCase
             $id, $imapUid, $calendarIntendedFor, $origId, $draftReplyType, $identityId, $draftAccountId, $draftAutoSendTime, $sentDate, $resentDate, $part, $fragment, [$email], $subject, $messageIdHeader, $inReplyTo, $invite, [$header], [$mp], [$shr], [$dlSubs]
         );
 
-        $response = new RemoveAttachmentsResponse($msgInfo);
+        $response = new SaveDraftResponse($msgInfo);
         $this->assertSame($msgInfo, $response->getMsgMessage());
-        $response = new RemoveAttachmentsResponse($chatInfo);
+        $response = new SaveDraftResponse($chatInfo);
         $this->assertSame($chatInfo, $response->getChatMessage());
-        $response = new RemoveAttachmentsResponse();
+        $response = new SaveDraftResponse();
         $response->setMessage($chatInfo);
         $this->assertSame($chatInfo, $response->getChatMessage());
         $response->setMessage($msgInfo);
         $this->assertSame($msgInfo, $response->getMsgMessage());
 
-        $body = new RemoveAttachmentsBody($request, $response);
+        $body = new SaveDraftBody($request, $response);
         $this->assertSame($request, $body->getRequest());
         $this->assertSame($response, $body->getResponse());
-        $body = new RemoveAttachmentsBody();
+        $body = new SaveDraftBody();
         $body->setRequest($request)
             ->setResponse($response);
         $this->assertSame($request, $body->getRequest());
         $this->assertSame($response, $body->getResponse());
 
-        $envelope = new RemoveAttachmentsEnvelope($body);
+        $envelope = new SaveDraftEnvelope($body);
         $this->assertSame($body, $envelope->getBody());
-        $envelope = new RemoveAttachmentsEnvelope();
+        $envelope = new SaveDraftEnvelope();
         $envelope->setBody($body);
         $this->assertSame($body, $envelope->getBody());
 
@@ -136,10 +167,19 @@ class RemoveAttachmentsTest extends ZimbraTestCase
 <?xml version="1.0"?>
 <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:urn="urn:zimbraMail">
     <soap:Body>
-        <urn:RemoveAttachmentsRequest>
-            <urn:m id="$id" part="$partIds" />
-        </urn:RemoveAttachmentsRequest>
-        <urn:RemoveAttachmentsResponse>
+        <urn:SaveDraftRequest wantImapUid="true" wantModSeq="true">
+            <urn:m aid="$id" origid="$origId" rt="r" idnt="$identityId" su="$subject" irt="$inReplyTo" l="$folderId" f="$flags" id="$intId" forAcct="$draftAccountId" t="$tags" tn="$tagNames" rgb="$rgb" color="$color" autoSendTime="$autoSendTime">
+                <urn:header name="$name">$value</urn:header>
+                <urn:content>$content</urn:content>
+                <urn:mp ct="$contentType" content="$content" ci="$contentId" />
+                <urn:attach aid="$id" />
+                <urn:inv method="$method" compNum="$componentNum" rsvp="true" />
+                <urn:e a="$address" t="t" p="$personal" />
+                <urn:tz id="$id" stdoff="$tzStdOffset" dayoff="$tzDayOffset" />
+                <urn:fr>$fragment</urn:fr>
+            </urn:m>
+        </urn:SaveDraftRequest>
+        <urn:SaveDraftResponse>
             <urn:m id="$id" i4uid="$imapUid" cif="$calendarIntendedFor" origid="$origId" rt="r" idnt="$identityId" forAcct="$draftAccountId" autoSendTime="$draftAutoSendTime" sd="$sentDate" rd="$resentDate" part="$part" xmlns:urn="urn:zimbraMail">
                 <urn:fr>$fragment</urn:fr>
                 <urn:e a="$address" d="$display" p="$personal" t="t" isGroup="true" exp="true" />
@@ -165,11 +205,11 @@ class RemoveAttachmentsTest extends ZimbraTestCase
                     <urn:content>$content</urn:content>
                 </urn:dlSubs>
             </urn:m>
-        </urn:RemoveAttachmentsResponse>
+        </urn:SaveDraftResponse>
     </soap:Body>
 </soap:Envelope>
 EOT;
         $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($envelope, 'xml'));
-        $this->assertEquals($envelope, $this->serializer->deserialize($xml, RemoveAttachmentsEnvelope::class, 'xml'));
+        $this->assertEquals($envelope, $this->serializer->deserialize($xml, SaveDraftEnvelope::class, 'xml'));
     }
 }
---
+++ tests/Mail/Struct/SaveDraftMsgTest.php
@@ -0,0 +1,114 @@
+<?php declare(strict_types=1);
+
+namespace Zimbra\Tests\Mail\Struct;
+
+use JMS\Serializer\Annotation\XmlNamespace;
+
+use Zimbra\Common\Enum\AddressType;
+use Zimbra\Common\Enum\ReplyType;
+
+use Zimbra\Mail\Struct\AttachmentsInfo;
+use Zimbra\Mail\Struct\CalTZInfo;
+use Zimbra\Mail\Struct\EmailAddrInfo;
+use Zimbra\Mail\Struct\Header;
+use Zimbra\Mail\Struct\InvitationInfo;
+use Zimbra\Mail\Struct\MimePartInfo;
+use Zimbra\Mail\Struct\SaveDraftMsg;
+use Zimbra\Tests\ZimbraTestCase;
+
+/**
+ * Testcase class for SaveDraftMsg.
+ */
+class SaveDraftMsgTest extends ZimbraTestCase
+{
+    public function testSaveDraftMsg()
+    {
+        $id = $this->faker->word;
+        $intId = $this->faker->randomNumber;
+        $origId = $this->faker->uuid;
+        $replyType = ReplyType::REPLIED();
+        $identityId = $this->faker->uuid;
+        $subject = $this->faker->text;
+        $inReplyTo = $this->faker->uuid;
+        $folderId = $this->faker->uuid;
+        $flags = $this->faker->word;
+        $content = $this->faker->text;
+        $fragment = $this->faker->text;
+        $contentType = $this->faker->word;
+        $contentId = $this->faker->uuid;
+        $name = $this->faker->name;
+        $value = $this->faker->word;
+        $method = $this->faker->word;
+        $componentNum = $this->faker->randomNumber;
+        $address = $this->faker->email;
+        $personal = $this->faker->word;
+        $tzStdOffset = $this->faker->randomNumber;
+        $tzDayOffset = $this->faker->randomNumber;
+
+        $draftAccountId = $this->faker->uuid;
+        $tags = $this->faker->word;
+        $tagNames = $this->faker->word;
+        $rgb = $this->faker->hexcolor;
+        $color = $this->faker->numberBetween(0, 127);
+        $autoSendTime = $this->faker->unixTime;
+
+        $header = new Header($name, $value);
+        $mimePart = new MimePartInfo($contentType, $content, $contentId);
+        $attachments = new AttachmentsInfo($id);
+        $invite = new InvitationInfo($method, $componentNum, TRUE);
+        $emailAddress = new EmailAddrInfo($address, AddressType::TO(), $personal);
+        $timezone = new CalTZInfo($id, $tzStdOffset, $tzDayOffset);
+
+        $msg = new StubSaveDraftMsg(
+            $id, $origId, $replyType, $identityId, $subject, [$header], $inReplyTo, $folderId, $flags, $content, $mimePart, $attachments, $invite, [$emailAddress], [$timezone], $fragment, $intId, $draftAccountId, $tags, $tagNames, $rgb, $color, $autoSendTime
+        );
+        $this->assertSame($intId, $msg->getId());
+        $this->assertSame($draftAccountId, $msg->getDraftAccountId());
+        $this->assertSame($tags, $msg->getTags());
+        $this->assertSame($tagNames, $msg->getTagNames());
+        $this->assertSame($rgb, $msg->getRgb());
+        $this->assertSame($color, $msg->getColor());
+        $this->assertSame($autoSendTime, $msg->getAutoSendTime());
+
+        $msg = new StubSaveDraftMsg(
+            $id, $origId, $replyType, $identityId, $subject, [$header], $inReplyTo, $folderId, $flags, $content, $mimePart, $attachments, $invite, [$emailAddress], [$timezone], $fragment
+        );
+        $msg->setId($intId)
+            ->setDraftAccountId($draftAccountId)
+            ->setTags($tags)
+            ->setTagNames($tagNames)
+            ->setRgb($rgb)
+            ->setColor($color)
+            ->setAutoSendTime($autoSendTime);
+        $this->assertSame($intId, $msg->getId());
+        $this->assertSame($draftAccountId, $msg->getDraftAccountId());
+        $this->assertSame($tags, $msg->getTags());
+        $this->assertSame($tagNames, $msg->getTagNames());
+        $this->assertSame($rgb, $msg->getRgb());
+        $this->assertSame($color, $msg->getColor());
+        $this->assertSame($autoSendTime, $msg->getAutoSendTime());
+
+        $xml = <<<EOT
+<?xml version="1.0"?>
+<result aid="$id" origid="$origId" rt="r" idnt="$identityId" su="$subject" irt="$inReplyTo" l="$folderId" f="$flags" id="$intId" forAcct="$draftAccountId" t="$tags" tn="$tagNames" rgb="$rgb" color="$color" autoSendTime="$autoSendTime" xmlns:urn="urn:zimbraMail">
+    <urn:header name="$name">$value</urn:header>
+    <urn:content>$content</urn:content>
+    <urn:mp ct="$contentType" content="$content" ci="$contentId" />
+    <urn:attach aid="$id" />
+    <urn:inv method="$method" compNum="$componentNum" rsvp="true" />
+    <urn:e a="$address" t="t" p="$personal" />
+    <urn:tz id="$id" stdoff="$tzStdOffset" dayoff="$tzDayOffset" />
+    <urn:fr>$fragment</urn:fr>
+</result>
+EOT;
+        $this->assertXmlStringEqualsXmlString($xml, $this->serializer->serialize($msg, 'xml'));
+        $this->assertEquals($msg, $this->serializer->deserialize($xml, StubSaveDraftMsg::class, 'xml'));
+    }
+}
+
+/**
+ * @XmlNamespace(uri="urn:zimbraMail", prefix="urn")
+ */
+class StubSaveDraftMsg extends SaveDraftMsg
+{
+}
