c3cf9c897e6beb0577f48e3b1eb88dfc8593f7ba
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-07-27 10:02:47 +0700

    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- src/Common/Soap/AbstractApi.php
+++ src/Common/Soap/AbstractApi.php
@@ -12,7 +12,6 @@ namespace Zimbra\Common\Soap;
 
 use JMS\Serializer\SerializerInterface;
 use Psr\Log\{LoggerAwareInterface, LoggerInterface, NullLogger};
-use Zimbra\Common\Enum\AccountBy;
 use Zimbra\Common\Serializer\SerializerFactory;
 use Zimbra\Common\Soap\Header\{AccountInfo, Context};
 
@@ -167,18 +166,6 @@ abstract class AbstractApi implements ApiInterface, HeaderAwareInterface, Logger
              ->getContext()
              ->setAccount($account);
         return $this;
-    }
-
-    /**
-     * Set target account to soap request header.
-     *
-     * @param string $account
-     * @return self
-     */
-    public function setTargetAccountByNameOrId(string $account)
-    {
-        $by = filter_var($account, FILTER_VALIDATE_EMAIL) ? AccountBy::NAME() : AccountBy::ID();
-        return $this->setTargetAccount(new AccountInfo($by, $account));
     }
 
     /**
