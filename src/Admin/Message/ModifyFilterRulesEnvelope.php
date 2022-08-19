82a210977b305d8f8ef11c2b6138b081a69f7fa7
Nguyen Van Nguyen <nguyennv1981@gmail.com>
2022-08-19 10:34:00 +0700

    WIP
    Signed-off-by: Nguyen Van Nguyen <nguyennv1981@gmail.com>

--- src/Admin/Message/ModifyFilterRulesEnvelope.php
+++ src/Admin/Message/ModifyFilterRulesEnvelope.php
@@ -26,7 +26,7 @@ use Zimbra\Common\Struct\{SoapBodyInterface, SoapEnvelope, SoapHeaderInterface};
  * @XmlRoot(name="soap:Envelope")
  */
 #[XmlNamespace(uri: 'urn:zimbraAdmin', prefix: 'urn')]
-#[XmlNamespace(uri: 'urn:zimbraAdmin', prefix: 'urn1')]
+#[XmlNamespace(uri: 'urn:zimbraMail', prefix: 'urn1')]
 #[XmlRoot(name: 'soap:Envelope')]
 class ModifyFilterRulesEnvelope extends SoapEnvelope
 {
